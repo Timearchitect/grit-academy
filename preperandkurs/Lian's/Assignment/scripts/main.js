abe5e2d3c28b45f59c12960b04b87bc50f3aa536
Takikato09 <liandelacruz09@hotmail.com>
2021-08-05 17:31:57 +0200

    1st uppgift
    :)

---
+++ preperandkurs/Lian's/Assignment/scripts/main.js
@@ -0,0 +1,12 @@
+console.log('welcome');
+let foo = 0;
+let bar = 0;
+
+console.log(foo/bar);
+
+if(foo/bar === 5){
+console.log ('Then it is!');
+
+} else {
+    console.log('You can\'t divide us!');
+}
