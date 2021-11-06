9e04f5d2e06ae895453a64b250b2318e6a4fe9b2
Alrik He <alrik_he@hotmail.com>
2021-11-06 12:28:24 +0100

    Simon tillfälle 1

---
+++ preperandkurs/demo/tillfälle 1 Simon/scripts.js
@@ -0,0 +1,59 @@
+/*
+alert("hello world");
+document.write("hello world");
+console.log("hello world");
+*/
+
+// deklarera en variabel foo
+let foo, amountOfApples = 456.001  ; 
+
+//tilldela värdet 5 till foo
+foo = 5;
+
+foo = 10;
+
+// teknik för att öka med 1
+foo = foo + 1;
+
+// alternativ med öka med 1
+foo += 1;
+
+// ökar bara med 1
+foo ++;
+
+
+foo = "hello";
+
+//deklarera variabel + tilldelning av 10
+let bar = 10;
+
+console.log( amountOfApples );
+
+
+// Primära Datatyper
+            // byte    
+bar = -12;  // integer,     Heltal      (int)
+            // long
+bar = 0.5;  // float,       decimaltal  (float)
+            // double
+bar = 'c';  // character,   karaktär    (char)  
+bar = false; // boolean,     boolean     (bool) 
+
+bar = "text"; // String,      sträng      (string,str)
+
+
+//[variabel]
+let foo1 = 5;
+
+//         [Array]
+// index     0   1      2     3       4
+// length 5 = antal element
+let foo2 = [ 5 , 0.5 , 'c' , true , "hello"] ;
+
+let que = ["Simon","Emma","Erik","Sara","Hadil","Alrik"];
+console.log(que );
+
+
+
+
+
