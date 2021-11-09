798fddeb066e74e9ae079dc9c0fe495b5dbb26dd
Alrik He <alrik_he@hotmail.com>
2021-11-09 19:02:41 +0100

    tillfälle 2

---
+++ preperandkurs/demo/tillfälle 1 Petra Tomo Marigona Adel/script.js
@@ -0,0 +1,65 @@
+
+/*alert("hello world");  //alertbox med ok knapp
+document.write("Hello world");
+ console.log(345678987654);*/
+/*
+// variabel är en behållare med ett namn, som kan innehålla värden
+// syntaxen = grammatiken
+// let var const -> namn -> ;
+// skapar en variabel = deklarera en variabel med namnet foo.
+let bar = 5.521416;
+let foo = "hello";
+
+bar = 1;
+// refererar till variabeln foo & sätter in värdet 5 i foo.
+// tilldela variabeln foo med värdet 5;
+// håll in alt + tab för att byta tab
+
+// teknik för att öka en variabel
+bar = bar + 1;
+
+bar += 1;
+
+bar ++;
+
+console.log( bar );
+
+
+
+// primära datatyper 
+                  // byte
+bar = -5;         // integer: heltal        (int)
+                  // long 
+bar = false;    // boolean  : boolean       (bool)
+bar = 'a';      // character: karaktär      (char)
+bar = -0.5;       // float  : decimaltal    (float,f)
+                // double
+//  datatype
+bar = "text";   // String   : Sträng        (String,Str) char[] array   
+
+*/
+
+// Array 
+//deklarera variabel + tilldelning
+let foo = 0; 
+
+//deklarera array  + tilldelning
+// index     0      1       2
+// element   1      2       3
+let foo2 = [ 0 , "hello" , true ];
+//foo2[0] = 999999; // tilldelare till en låda 0 låda
+
+//foo2 = 123; // efter till delning , är foo2 en variabel
+//foo2 = [1,2,34,"hello"]; // efter till delning , är foo2 en array
+//foo2.pop();
+//foo2.pop();
+//foo2.pop();
+//foo2.push(0.5);
+
+//foo2.push("hejsan");
+
+let que = ["Petra","Tomo","Adel","Alrik","Marigona"];
+
+
+
+
