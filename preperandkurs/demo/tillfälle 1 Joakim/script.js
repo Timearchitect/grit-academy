663a6b40f66e5cb14f47548041f7ea8623000974
Alrik He <alrik_he@hotmail.com>
2021-11-12 16:29:37 +0100

    tillfälle 1 med joakim

---
+++ preperandkurs/demo/tillfälle 1 Joakim/script.js
@@ -0,0 +1,61 @@
+/*
+alert("Hello world");
+console.log("Hello world");
+document.write("Hello world");
+
+
+//Variablar
+//Deklarera variabel
+let foo;
+
+//Tilldelar värdet
+foo = 5;
+
+//Deklarera variabel + tilldelning
+let bar = 2;
+
+foo = "hejsan"+ 1 + 1 ;
+
+console.log( foo + bar );
+
+
+//deklarerar variabel 
+let foo1 = "hej";
+foo1= 6;
+
+//deklarerar array , den har 3 element 
+//  index     0    1    2     
+let foo2 = ["hej", 5 , 0.2 ];
+
+//foo2.push(0);
+//foo2.push("hejsan");
+foo2.pop();
+
+
+*/
+
+// Datatyper
+
+let bar;
+// Number
+                // byte    (byte)      : Byte
+bar = 1;        // integer (int)       : Heltal
+                // long    (long)      : Long
+
+bar = 0.5;      // float    (float)     : Decimaltal
+                // double   (double)    : Dubbel    
+
+bar = 'c';      // character(char)      : Karaktär
+
+bar = true;     // boolean  (bool)      : Boolean
+
+// icke primär datatyp
+bar = "hejsan"; // String   (String,Str,char[]): Sträng
+
+
+
+
+
+
+
+
