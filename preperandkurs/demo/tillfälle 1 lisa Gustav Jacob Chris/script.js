240b67f5cb29437653e39f7435ff237a21cfa304
Alrik He <alrik_he@hotmail.com>
2021-11-17 19:10:33 +0100

    demo 1

---
+++ preperandkurs/demo/tillfälle 1 lisa Gustav Jacob Chris/script.js
@@ -0,0 +1,86 @@
+/*
+console.log("Hejsan");
+
+document.write("Tjena!");
+
+alert("HEllo");
+
+
+
+
+// Variabler = tom box som kan ha värden i sig
+// Deklarera en variabel ,  skapar varibeln
+// let mellanslag namn    
+let foo;
+// Deklarerar + tilldelar samtidigt.
+let bar = 8;
+// Tilldela variabel med värdet 5
+foo = 5;
+console.log( foo );
+
+foo = "TJENARE!!";
+console.log( foo );
+
+foo = 2 + 2 * 3;
+console.log( foo );
+// skriver ut till konsolen
+
+foo = "hej     " + "tjena";
+console.log( foo  );
+
+
+foo = "hej" + 1;
+console.log( foo  );
+
+
+foo = 3+1+1+1+1+1+1+5+5+5;
+console.log( foo );
+
+
+//Datatyper
+// typer av värden
+
+                //Number
+
+                //byte
+bar = 12;       //integer    ,heltal (int)
+                //long
+                
+bar = 0.5;      //float      ,decimaltal (float,f)
+                //double
+// Text
+
+bar = 'c';      //character  ,karaktär (char) 
+
+bar = "Hello";  //String     ,Sträng/text (String,Str,char[])
+
+bar = true;     //boolean   ,boolean (bool)
+
+*/
+
+// Array
+
+//variabel 
+let foo1; 
+foo1 = 5;
+
+let foo2; 
+// index  0    1    2      3 
+foo2 =  [ 5 ,"hello",true, 0.2458 ];
+
+// tilldelning till bara elementet
+foo2[0] = 999;
+
+// tilldelning variabel skiver över allt.
+foo2 = 5;
+
+let que = ["Sana","Jacob","Chris","Alrik"];
+
+console.log(que.length);
+
+
+
+
+
+
+
