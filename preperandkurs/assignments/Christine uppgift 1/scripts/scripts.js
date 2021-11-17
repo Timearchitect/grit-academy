bb422a1693470653db08a995ac9f6c01983a3e71
Alrik He <alrik_he@hotmail.com>
2021-11-17 14:58:18 +0100

    moved folders

--- preperandkurs/assignments/Christine uppgift 1/scripts/scripts.js
+++ preperandkurs/assignments/Christine uppgift 1/scripts/scripts.js
@@ -1,11 +1,98 @@
+// (condition) påstående/regel
+//  sant / falskt
+//vid sant kör nästa kod
+//vid falskt skip nästa kod
+/*
+let amountOfApples;
+amountOfApples = 15;
 
- let foo = 0;
+// == lika    != inte lika med
+//   <   mindre än  > större än
+//  <=   mindre+lika  >= större+lika
 
- let bar = 0;
- 
- if(bar/foo == Number){}
- else 
+if( false )
+    console.log("SANT");
+else 
+    console.log("FALSE");
 
- console.log("you cant divide us");
+
+// första FALSKT sedan går den vidare
+if( false )
+    console.log("större än 10");
+else if( amountOfApples < 20 )
+    console.log("mindre än 20");
+else 
+    console.log("Nej inte < 20");  
+
+
+// första SANT sedan går den vidare
+if( true )
+    if( amountOfApples < 20 )
+        console.log("båda är sanna");
+else  
+    console.log("nej");
+
+
+let age= 16;
+let vip = true;
+// && och    || eller
+
+
+if(  age >= 18   &&  vip == true  )
+    console.log("SANT!!!!!!!!");
+else 
+    console.log("FALSE");
+
+
+
+
+// else kan bara existera under en if()
+
+console.log("vidare...");
+
+
+
+// while loop
+
+// (condition) regel/påstående
+// vid sant så repeterarden
+// vid falskt stoppar den och går vidare
+let j = 0;
+while(j < 3){
+    //document.write("<button>knapp</button>");
+    console.log( j + " while loop");
+    j = j + 1; 
+}
+
+// do while loop  
+// (condition) är efter koden som repeteras
+// en loop minimun  
+let i = 0;
+do{
+    console.log(i + " do-while loop");
+    i = i + 1; 
+}
+while(i < 3)
+
+
+// for loop
+// (   variabel  ;   condition  ;  ökar  )
+for( let i = 0;     i < 3 ;    i += 1  ){
+    console.log(i + " for loop");
+}
+
+
+
+let que = ["Alrik","Petra","Adel","Helen"];
+
+for( let i = 0;     i < 4 ;    i += 1  ){
+    let text= 'kö plats '+ i +' : '+ que[i];
+    document.write("<div>"+text+"</div>");
+}
+
+*/
+
+
+console.log("vidare...");
 
 
