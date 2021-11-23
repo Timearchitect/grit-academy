72dd70db4160d4a56b58f8e5c47edb40853094f0
Alrik He <alrik_he@hotmail.com>
2021-11-23 18:03:12 +0100

    changes

--- preperandkurs/assignments/Petra/Uppgift 1/Scripts/script.js
+++ preperandkurs/assignments/Petra/Uppgift 1/Scripts/script.js
@@ -1,5 +1,5 @@
 
-let foo = 0;
+/*let foo = 0;
 let bar = 0;
 
 if(bar/foo == Number){
@@ -7,3 +7,110 @@ if(bar/foo == Number){
 }
 else
 console.log("You can't divide us!");
+
+
+*/
+
+
+
+
+
+// Funktioner
+// call function alert  med parametern "hejsan"
+//alert( "Hejsan" );
+
+
+let alrik; // deklarera variabel
+alrik = "hej";
+
+function alriksFunktion( x , y , z ){} 
+
+
+
+
+
+// Objekt
+
+// variabel
+let foo = 5;
+foo = 10;
+
+//array
+//index          0        1            2                3 
+let foo2 = [     5  ,       true ,       "Green" ,      65.5   ];
+foo2[0] = 10;
+
+// object
+// egenskaper   id         ready        color         tank  
+//          namn : värde
+let foo3 = {    id : 5 ,  ready : true , color : "Red" , tank: 65.5  };
+foo3.id = 10;
+
+let Bil = {
+    regnummer: "152OBS",
+    antalHjul: 4 ,
+    start: function (){ 
+        console.log("BRRRMM~~~~");
+    }
+}
+
+
+
+function log( para1  ){
+    return para1;
+} 
+
+
+
+let alriksConsole = {   log : 
+
+    function( para1 ){
+        console.log( para1 );
+    } 
+
+};
+
+
+
+
+// Metoder
+console.log();
+
+
+// identifiera funktion parantes () och ;
+
+
+
+
+
+
+
+
+
+
+// Objekt
+2222222
+
+let store = {
+    name: "ICA",
+    openingHours: "09:00-20:00",
+    isOpened: true,
+    rentPerMonth: 25000
+}
+
+store.openingHours = "08:00-21:00" 
+
+console.log(store);
+
+store["openingHours"] = "08:00-22:00" 
+
+console.log(store);
+
+Object.values(store)[1] = "08:00-23:00" 
+
+console.log(store);
+
+2222
+
+
+
