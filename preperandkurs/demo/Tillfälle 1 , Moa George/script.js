564e7b131b20d90b1d19dab65f2bbb4eb6f69486
Alrik He <alrik_he@hotmail.com>
2021-11-29 18:51:11 +0100

    tillfälle 1

---
+++ preperandkurs/demo/Tillfälle 1 , Moa George/script.js
@@ -0,0 +1,89 @@
+/*
+alert("hello world");
+console.log("Hejsan");
+document.write("hej");
+
+
+
+// Variablar , en behållare med en label
+// deklarera  , skapar en variabel
+let foo;
+
+// tilldela , lägger in ett värde
+foo = 5;
+
+foo = 3 + 1 +1 +1 +1 +1;
+
+// printar ut i consolen
+
+//deklarera + tilldela i samma rad
+let bar = 0;
+
+// tilldela sig själv + 1
+bar = bar + 1;
+// öka med 1;
+bar += 1;
+// öka bara med 1
+bar ++;
+
+console.log( bar ) ;
+
+
+
+// Datatyper
+let bar;
+                //byte
+bar = 5;        //integer   ,heltal     (int)
+                //long
+
+
+bar = 0.5;      //float     ,decimaltal (float,f)
+                //double
+
+bar = 'c';      //character ,karaktär   (char)
+
+bar = false;    //boolean   ,boolean    (bool)
+
+
+bar = "hej";    //String    ,Sträng text(Str,String)
+
+*/
+
+
+// ------ Arrayer -----
+
+// deklarera variabel
+let foo1;
+// tilldelar värdet 5
+foo1 = 5; 
+
+
+
+// deklarera variabel
+let foo2;
+// tilldelar en array med 5
+//index  0      1       2      3      4
+foo2 = [ 5  , 0.001, "hello", true , 'ö' ]; 
+
+// tarbort sista elementet
+foo2.pop();
+
+//lägg till element på slutet med "hello"
+foo2.push("hello");
+
+console.log(foo2.length);
+
+
+let foo3 = "hello!!!";
+
+
+
+
+
+
+
+
+
+
+
+
