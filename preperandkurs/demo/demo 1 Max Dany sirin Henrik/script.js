0a2507eb284cd300bdca5dca2f6218ae2e308031
Alrik He <alrik_he@hotmail.com>
2021-12-03 18:42:40 +0100

    demo 1 med max och Dany

---
+++ preperandkurs/demo/demo 1 Max Dany sirin Henrik/script.js
@@ -0,0 +1,59 @@
+/*
+
+alert("Hello world");
+document.write("Hej");
+console.log("Hejsan!!!");
+
+
+
+
+//variabler
+//Deklarera en variabel
+let foo;
+
+//Tilldela variabeln foo med 5
+foo = 5;
+
+//deklarera + tilldela i en rad
+let bar = 10;
+
+foo = 300;
+
+bar = "hej" + (1 + 1) ;
+
+console.log( bar );
+
+
+let bar;
+// Datatyper
+
+//Number i JS
+                //byte, long
+bar = 1;        //integer   ,   heltal (int)
+
+                //double
+bar = 0.5;      //float     ,   decimaltal (float,f)
+
+
+//String i Js
+bar = 'c';      //character ,   karaktär    (char)
+bar = "text";   //String    ,   Sträng      (String,Str)
+
+bar = false;    //boolean   ,   boolean     (bool)
+
+*/
+
+
+// Arrayer
+//deklaration av variabel
+let foo;
+foo = 5; 
+
+// deklaration av en array
+// index     0      1        2         3      4
+// foo2 har 5 element
+let foo2; 
+foo2  = [ 5 , "hello" , true , 0.534501 , '@' ];
+
+
+let que = ["Max","Sirin","Jacob","Dany","Alrik"];
---
+++ preperandkurs/demo/demo 1/scripts.js
@@ -0,0 +1,62 @@
+/*
+alert("hello world");
+console.log("hello world");
+document.write("hello world");
+*/
+
+// kommentar
+
+// deklarera en variabel
+let foo; 
+
+// tilldela variabeln foo med 5 
+foo = 5;
+
+foo = foo + 1;
+
+//  pulsar och ändrar 
+foo += 1;
+
+// ökar med 1
+foo ++;
+
+
+
+foo = "hej";
+
+//deklarerar samt tilldalar 0 till bar
+let bar = 0;
+
+console.log( bar ); 
+
+//datatyper
+
+bar = false;// Boolean      :   boolean     (bool)
+bar = -2;   // heltal       :   integer     (int) 
+bar = 'c';  // Karaktär     :   character   (char)
+
+                // long 
+bar = 0.526;// Deciamltal   :   float       (float)
+                //double
+
+bar = "text"// Text         :   String      (String)
+
+// variabel
+let foo1 = 5;
+
+
+// array
+//  index    0     1        2       3       4   
+// varje värde är ett element
+// []  är arrayen.
+let foo2 = [ 5 , 5.222 , "hello" , true , false ];
+// när foo2 är tilldelad då klass den som en array
+foo2[3] = 9;
+
+
+//let que =  ["Julia" ,"Alrik","Jan","Kalle","Amanda","Jean"];
+
+console.log( foo2[2] );
+
+
+
