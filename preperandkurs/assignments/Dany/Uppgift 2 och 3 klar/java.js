01dbd1f3bd0b4ea5bf456c8d1148f63374d2a8b5
Alrik He <alrik_he@hotmail.com>
2021-12-13 14:57:51 +0100

    nicce

--- preperandkurs/assignments/Dany/Uppgift 2 och 3 klar/java.js
+++ preperandkurs/assignments/Dany/Uppgift 2 och 3 klar/java.js
@@ -1,6 +1,31 @@
-let cat = ["Gustav" , true ];
-let mjau = { name: "Gustavo" , alive:true} 
+let cat = { 
+    name: "Gustavo" ,
+    alive:true,
+    changeNameOnCat: function(x){
+        cat.name = x;
+    }
+    
+    }; 
+
 
 function kitkat(){
-    alert( "Gustavo" );
+    alert( cat.name );
 }
+
+
+
+
+/*
+ 
+// finns det något funktionellt fel / logiskt fel
+
+let age = 15;
+
+if(age > 5)
+    console.log("big");
+else if(age > 10)
+    console.log("bigger");
+else
+    console.log("small");
+
+*/
