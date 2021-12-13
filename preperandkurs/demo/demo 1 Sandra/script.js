c29733ff149f3649462be53cd81ea625864902c1
Alrik He <alrik_he@hotmail.com>
2021-12-13 22:26:38 +0100

    sandras  tillfälle 1

---
+++ preperandkurs/demo/demo 1 Sandra/script.js
@@ -0,0 +1,48 @@
+/*
+alert("Tjena");
+document.write("Hej"); 
+console.log("hello");
+ */
+
+// variabler 
+// deklarera en variabel med namnet foo
+let foo; 
+
+// tilldela variabeln foo med 5
+foo = 9;
+
+foo = foo + 2;  // öka en variabel med 1 
+foo += 2;       // ökar med 1
+foo ++;         // ökar med bara 1 
+
+// + - * / ( ) 
+
+console.log(  "hej"  );
+
+let bar;
+// Datatyper
+
+// number
+bar = 5;        //integer,  heltal      (int)
+bar = -0.01;    //float,    decimaltal  (float,f)
+
+// string
+bar = 'a';      //character,karaktär    (char)
+bar = "text";   //String   ,sträng/text (String,str)
+
+// boolean
+bar = true;     //boolean   ,boolean    (bool)
+
+
+
+// Array
+let foo1;
+foo1 = 5;
+
+
+
+let foo2;
+// index    0      1      2     3
+foo2 =    [ 5 , "hello", 0.5 , true  ];  // <---  array []
+foo2[0] = foo2[1] + foo2[3] ;
+// finns fler saker
