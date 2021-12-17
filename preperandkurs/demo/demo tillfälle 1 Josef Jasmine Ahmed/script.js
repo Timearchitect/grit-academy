c80c41d3053a7a3dfd215b81a4a6ce36090a4a4e
Alrik He <alrik_he@hotmail.com>
2021-12-17 21:08:41 +0100

    demo 1 2021-12-17

---
+++ preperandkurs/demo/demo tillfälle 1 Josef Jasmine Ahmed/script.js
@@ -0,0 +1,61 @@
+/*
+alert("hello");
+document.write("Hejsan");
+console.log("Hej");
+
+
+
+// demo 1 med Jasmine , Josef & Ahmed
+
+
+//Variabler
+//syntax
+//Deklarera en variabel med namnet foo (skapa)
+let foo;
+
+//Tilldelning foo med värdet 5  (skriva över)
+foo = 5;
+foo = "hello";
+//deklaration + tilldelning i samma rad
+
+let bar = 2;
+
+bar = bar + 2;   // öka med 2
+bar += 2;        // öka med 2
+bar ++;          // ökar bara med 1
+ 
+
+
+// Datatyper
+// definitioner
+
+//            Number
+bar = 5;    //integer   ,heltal(int)
+bar = 0.1;  //float     ,decimaltal(float,f)
+
+//            String
+bar = '#';  //char      ,karaktär(char)
+bar = "hej";//String    ,sträng(String,str)
+ 
+//             boolean
+bar = false; //boolean   ,boolean(bool)
+*/
+
+//Array
+//Syntax 
+let foo = 5;
+
+
+//  index   0     1      2     3
+let bar = [ 5 , "hej" , 0.5 , true];
+//  element
+
+// tilldela inuti arrayn
+bar[3] = 1;
+
+console.log( foo + bar[1] );
+
+
+
+
+
