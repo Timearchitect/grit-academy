8fdf25910627bc86e07d01733eaeea68322c9f13
Alrik He <alrik_he@hotmail.com>
2021-12-20 18:58:39 +0100

    demo 1

---
+++ preperandkurs/demo/demo 1 erika Jaqueline Noak/script.js
@@ -0,0 +1,60 @@
+/*
+alert( "hello" );
+document.write("Tjena");
+console.log("Hej!");
+
+
+// (1) Variabler
+//container med ett värde
+//syntax
+//skapa varibel så kallas det för att DEKLARERA
+//deklarerar en variabel
+let foo;
+
+//tilldela värdet 5
+foo = 5;
+
+//deklarering + tilldelning
+let bar = 5;
+
+bar = bar + 1; // ökar med 1
+bar += 1;      // ökar med 1
+bar ++;        // ökar bara med 1 
+
+
+// Datatyper
+//
+let bar;
+
+//Number
+bar = 5;    //integer   ,heltal     (int)
+bar = 0.5;  //float     ,decimaltal (float,f)   
+
+//String
+bar = 'c';  //character ,karaktär   (char)
+bar = "hej";//String    ,Sträng     (Str,String)
+
+//Boolean
+bar = true; //boolean   ,boolean    (bool)
+
+*/
+
+//Arrayer 
+
+let foo;                         //variabel
+foo = 5;                         //variabel
+
+let bar;                        //variabel   
+// element (varje låd är ett element)
+// index   0       1         2         3
+bar = [    1  ,   0.01  ,   "hej"  ,  true  ];   //array []
+bar[3] = "Tjena!!";
+
+let que = ["Jaqueline","Noak","Erika","Alrik"];
+
+
+
+
+
+
+
