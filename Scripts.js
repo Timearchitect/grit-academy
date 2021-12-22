00cc1b904106d0b57a40baa0a9de4b3fd34cb2b8
Noocce <96465940+Noocce@users.noreply.github.com>
2021-12-22 13:10:40 +0100

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,14 @@
+let foo = 0;
+let bar = 0;
+console.log( bar,foo );
+ 
+function log() {
+console.log('divide');
+ };
+ 
+console.log("Malmö FF");
+{
+if ( foo / bar == Number);
+ else
+alert("Framåt Malmö!");
+}
