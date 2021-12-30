c9989f9c8eea4386c3b9e1e89c5bf34e86722f4f
Alrik He <alrik_he@hotmail.com>
2021-12-30 16:37:05 +0100

    update

---
+++ preperandkurs/demo/demo 1_5 Sofie C/script.js
@@ -0,0 +1,77 @@
+/*
+
+//alert("hello");
+//console.log("hej");
+
+//Syntax/grammatik
+//Deklarerar en variabel med namnet foo
+//Skapa en variabel
+// unikt namn
+let foo;
+
+//att tilldela värdet 5 till foo
+foo = 5;
+
+foo = "hej";
+
+//deklarering + tilldelning i en rad
+let bar = 5;
+
+// operatorer/matte
+//hantera tilldelningar & uttryck
+foo = 2 + 2 * 4;
+foo = "hej" + " tjena";
+foo = "hej" + (1 + 1);
+console.log( foo + bar );
+
+let alrik = 888;
+let sofie = "hej";
+let gustav = 0;
+
+gustav = sofie + alrik;
+
+*/
+
+// Datatyper
+// klassifikationen
+
+let bar = false;
+
+// Number i js
+bar = 1;    //integer   ,heltal     (int)
+bar = 0.5;  //float     ,decimaltal  (float/f)
+
+// String i js
+bar = 'a';  //character ,karaktär   (char)
+bar = "hej";//String    ,Sträng     (String,Str)
+
+// boolean i js
+bar = true; //boolean   ,boolean    (bool)
+
+console.log(true + "hej");
+
+
+
+//Arrayer
+//syntax
+
+let foo; //deklarerar en variabel
+foo = 9; //tilldelar en 9
+
+
+let foo2; //deklarerar en variabel
+//index   0    1      2 
+//element
+foo2 = [ 9 , "hej", 0.1]; //tilldelar en array med 9
+//foo2 = 5; // tilldelar numer 5 (arrayen är borta)
+
+foo2[0] = 5;
+
+foo2.pop();
+foo2.push("hej");
+
+
+
+
+
+
