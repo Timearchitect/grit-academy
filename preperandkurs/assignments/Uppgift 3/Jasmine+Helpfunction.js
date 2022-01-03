9a2b25dccf54739dd719ad98c63fed792b86ade1
JasmineRon <Jasmine.ronnefors@hotmail.com>
2022-01-03 11:20:10 +0100

    Jasmine uppgift 3

---
+++ preperandkurs/assignments/Uppgift 3/Jasmine+Helpfunction.js
@@ -0,0 +1,7 @@
+
+
+function charRemove (char , text){
+    console.log (text.replaceAll(char , ""));
+}
+
+charRemove ("H" , "Hejsan")
---
+++ preperandkurs/assignments/Uppgift 3/cat.js
@@ -0,0 +1,7 @@
+
+let cat = {
+
+    alive: true,
+    name: "Gustav",
+
+}
