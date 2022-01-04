1fe88ebca99d82f321342c135d5704983ea2ce62
Lyckorosen <96499622+Lyckorosen@users.noreply.github.com>
2022-01-04 15:51:46 +0100

    Uppgift 1

---
+++ preperandkurs/assignments/Sofie/script instruktioner.js
@@ -0,0 +1,60 @@
+/*
+//alert ("hello");
+console.log ("hej")
+//deklarera en variabel foo
+
+//syntax/grammatik
+let foo;12 
+
+//att tilldela värdet 12
+foo=12
+//allting åt höger lägger in en 5:a i textfilen. Man har skrivit över akkting i nummret 5. Skriver över.  Checka och kolla vilket värde foo har. 
+console.log (foo);
+foo="hej"
+console.log (foo);
+//deklaringer + tilldelning i en rad 
+let bar =5;
+//operatorer/matter
+// hantera tilldeningar och utryck (mattematiskt problem)
+foo=(2*2)*4;
+foo="tjena"+" hej"
+foo= "hej"+ 1 + 1;
+foo= 1 + 1+ "hej";
+foo= "hej" + ( 1+ 1);
+console.log(foo + bar);
+
+// värdet i foo och bar är det viktigaste
+
+let alrik = 888;
+let sofie= "hej";
+let gustav= o;
+alrik=sofie;
+//bara en variabel här= okej med flera variabler här
+gustav= alrik+sofie;
+
+
+//datatyper
+//Klassifikationer
+//string= text 
+*/
+
+let bar; 
+bar=1;
+bar= 0,5; //float , decimaltal (float/f)
+
+
+//true och false= som on och off 
+
+//Arrayer
+let foo= 2; 
+
+foo = [9,"hej"]
+
+//längden är hur många variabler som är med. Punkt är till för deciamltal 
+
+Array= 
+
+foo 2 = []
+
+//Tilldela saker; 3 element med olika värden. TIlldela själva hyllan 
+foo 2= 
---
+++ preperandkurs/assignments/Sofie/script.js
@@ -0,0 +1,6 @@
+
+let foo=0;
+let bar=0;
+
+console.log("hej")
+console.log (foo/bar)
