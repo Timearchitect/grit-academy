52e631eb781b6047e9227bcd233d8a6b04bd7bfb
Alrik He <alrik_he@hotmail.com>
2022-03-29 00:15:24 +0200

    update

---
+++ preperandkurs/demo/tillfälle1 2022-03-28/script.js
@@ -0,0 +1,60 @@
+/*
+
+Tillfälle 1 Demo!!!
+
+*/
+
+document.write("YOYO");
+console.log("hello world");
+
+// Variabler är en behållare med ett värde
+// kod för att: deklarera en variabel med namnet foo
+let foo;  
+
+// tilldela variabeln foo med 999
+foo = 999;
+foo = 879;
+
+// deklaration med tilldelning
+let bar = 888;
+
+// prioriterings regel förljer matte
+bar = 2 * 2 + 1;
+
+console.log(1234567);
+console.log("1#3sa!7FFF");
+
+// när variabler används så används innehållet
+
+bar = 1 + 1 + "text";  // resulterar i "2text"
+bar = "text" + 1 + 1;  // resulterar i "text11"
+
+console.log(foo);  
+
+let sif = 1; 
+let t = "text";
+
+bar = t + sif + sif; 
+
+let pengar = 0;
+console.log(pengar);
+
+
+// Arrayer
+// variablar med flera slots
+// som en hylla med flera lådor
+
+let foo1;  
+foo1 = 888;
+
+let foo2; 
+foo2 = [5 , "hello" , 3]; //  <--- array 
+// när något tilldelas en [...] då blir den en array
+
+console.log( foo2[0] + foo2[2] );
+
+
+
+
+
+
