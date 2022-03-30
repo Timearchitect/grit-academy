e35244a863d34fa85b175c81cf470a236890c572
Jonassons <albin.jonasson00@outlook.com>
2022-03-30 13:36:12 +0200

    upg1
    upg1

---
+++ preperandkurs/assignments/scripts/.js
@@ -0,0 +1,24 @@
+//variablar 
+let foo;
+
+foo=0
+
+console.log(foo);
+
+let bar
+
+bar=0
+
+console.log(bar); 
+
+console.log(bar/foo);  //divideringen 
+
+if (foo/bar ==0)                     // alerten
+{
+}
+else
+alert ("you cant divide us");
+
+function log() {
+    console.log('divide');      // script för att logga divide knappen
+      };
