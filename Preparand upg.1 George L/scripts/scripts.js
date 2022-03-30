131f1630800b0c2ee8894aaae67148538bbd31dc
George Lindborg <95055829+Jojje0428@users.noreply.github.com>
2022-03-30 23:37:48 +0200

    Add files via upload
    George / Uppgift 1

---
+++ Preparand upg.1 George L/scripts/scripts.js
@@ -0,0 +1,18 @@
+console.log("Hi world");
+var foo=0;
+var bar=0;
+if(bar==0){
+
+console.log("You cant divide us!");
+
+}
+else{
+console.log(foo/bar);
+
+}
+
+
+
+function dividefunction(){
+console.log("divide");
+}
