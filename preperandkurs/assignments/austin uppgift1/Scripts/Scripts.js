205f422da54cd5654f5e574d014a438b0a5ad22a
Alrik He <alrik_he@hotmail.com>
2022-03-31 16:08:23 +0200

    flyttade saker

---
+++ preperandkurs/assignments/austin uppgift1/Scripts/Scripts.js
@@ -0,0 +1,17 @@
+function calldivide(){
+    console.log("divide")
+}
+
+let foo = 0;
+let bar = 0;
+
+let result = foo / bar;
+
+// if result equals to 0 or result exist
+if(result == 0 || result){
+    console.log("result = " + result)
+}
+else{   //else did not get any result (result = bar / foo)
+    console.log("Cannot divide us")
+}
+
