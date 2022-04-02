a34fabb0765f13b17c4e6ff35e4a803a65c9ef51
Alrik He <alrik_he@hotmail.com>
2022-04-02 16:31:42 +0200

    extracted

--- preperandkurs/assignments/Preparand upg.1 George L/scripts/scripts.js
+++ preperandkurs/assignments/Preparand upg.1 George L/scripts/scripts.js
@@ -1,7 +1,7 @@
 console.log("Hi world");
 var foo=0;
 var bar=0;
-if(bar==0){
+if(bar==0){  // inte riktigt korrect med vad som skulle hända, detta jämnför om bar är 0.
 
 console.log("You cant divide us!");
 
@@ -13,6 +13,6 @@ console.log(foo/bar);
 
 
 
-function dividefunction(){
+function dividefunction(){     // nice med funktionen som körs via knappen, man kan även ha console.log() i knappen direkt
 console.log("divide");
 }
---
+++ preperandkurs/assignments/uppgift 1 pontus/scripts/script.js
@@ -0,0 +1,13 @@
+alert ("you cant divide us")
+
+let namn = "divide";
+console.log (namn) 
+
+let foo;
+foo = 0
+
+let bar;
+bar = 0
+
+console.log ( foo / bar);  // nice 
+
