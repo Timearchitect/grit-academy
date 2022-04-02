624ce1f1f9effe706af444f07f3371f1935fc1f9
Alrik He <alrik_he@hotmail.com>
2022-04-03 00:12:56 +0200

    demo

---
+++ preperandkurs/demo/tillfäll2 2022-04-02/script.js
@@ -0,0 +1,103 @@
+
+let foo = 0;
+// datatypen number
+foo = 88;   //Integer   ,heltal
+foo = 0.5;  //Float     ,decimaltal
+
+// datatyp string
+foo = "hello";   //String,text/sträng
+foo = '@';       //char/character,karaktär
+
+// datatyp boolean
+
+foo = true;   //boolean , boolean ,1
+foo = false;  //                   0
+
+
+
+// If satser
+
+// condition
+// == jämnför
+// < mindre än
+// > större än
+// <= mindre eller lika med
+// >= större eller lika med
+
+// "hello" == "Hello" : falskt
+// 1.0 == 1 : sant
+// 1.0 === 1 : sant
+// "1" == 1 : sant
+// "1" === 1 : falskt
+// true == 1 : sant
+// true === 1 : falskt
+//let minimumAge = 18;
+//let ageOfThePerson = 18; 
+// ageOfThePerson == minimumAge
+// 1 == 6+6*0.5-8 : sant;
+
+/*
+// (1) separata if satser
+if ( 1 == 0 ){ // första
+    console.log("YES1");
+}
+else{
+    console.log("NO1");
+}
+
+
+if ( 1 == 1 ){ // andra
+    console.log("YES2");
+}
+else{
+    console.log("NO2");
+}
+
+
+
+// (2) om den första är sant kör den andra
+if ( 1 == 1 ){ // första
+    console.log("YES1");
+    
+    if ( 1 == 0 ){ // andra
+        console.log("YES2");
+    }
+    else{
+        console.log("NO2");
+    }
+
+}
+else{
+    console.log("NO1");
+}
+
+
+
+// (3) om den första är falskt kör den andra
+if ( 1 == 1 ){ // första
+    console.log("YES1");
+}
+else{
+    console.log("NO1");
+    if ( 1 == 0 ){ // andra
+        console.log("YES2");
+    }
+    else{
+        console.log("NO2");
+    }
+}
+*/
+//(4) 2 ifsatser i en
+// && and och   behöver 2 sant för att resultatet är sant
+// || or eller om 1 sida har sant då är resultatet sant 
+
+if ( false || false ){ 
+    console.log("YES!!!");
+}
+else{
+    console.log("NO!!!!");
+}
+
+
+
+//let foo2 = [ 88 , "hello"]
