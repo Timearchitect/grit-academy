43e65be82a97267290732c689c92de52451701ef
Jonassons <albin.jonasson00@outlook.com>
2022-04-03 18:11:11 +0200

    upg1_5albin

---
+++ preperandkurs/assignments/albinuppg1.5/script.js
@@ -0,0 +1,34 @@
+console.log("the club");
+
+let vip; 
+let age;
+age= 17; 
+
+{    
+
+        if (true)  alert("Access Granted");
+
+        else alert("Access Denied");
+}
+
+{
+        if (age < 18 )
+        console.log("true");
+}
+{
+        if(vip == true) alert("Welcome in my best VIP customer");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
