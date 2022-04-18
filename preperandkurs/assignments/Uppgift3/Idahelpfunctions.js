862c4faf9a72abe4462c432acf7481133efe102a
IdaRoos <102700726+IdaRoos@users.noreply.github.com>
2022-04-18 18:44:52 +0200

    Add files via upload

---
+++ preperandkurs/assignments/Uppgift3/Idahelpfunctions.js
@@ -0,0 +1,5 @@
+
+function charRemove(text, char) {
+    console.log(text.replaceAll("a",""));
+}
+console.log(charRemove("I liaaakeaaa doaaags"));
---
+++ preperandkurs/assignments/Uppgift3/cat.js
@@ -0,0 +1,1 @@
+let cat = { alive: true, name:"Gustav"};
