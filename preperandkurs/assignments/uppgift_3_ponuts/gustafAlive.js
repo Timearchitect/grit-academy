72dd4fc81d570465b46aa8ca832453970ba65d72
Alrik He <alrik_he@hotmail.com>
2022-05-02 17:53:06 +0200

    fixed assignment

---
+++ preperandkurs/assignments/uppgift_3_ponuts/gustafAlive.js
@@ -0,0 +1,10 @@
+function dead_or_alive(){
+    var cat = {"Name":"Gustav","Alive":true}
+    var dead_alive = Math.floor(Math.random() * 2)
+    if(dead_alive == 0){
+        return alert("The cats name is " + cat.Name + " and it´s dead, no more lasagne :(")
+    }
+    if(dead_alive == 1){
+        return alert("the cats name is " + cat.Name + " and it´s alive and will get more lasagne :)")
+    }
+}
---
+++ preperandkurs/assignments/uppgift_3_ponuts/pontusHelpfunction.js
@@ -0,0 +1,17 @@
+function charRemove (char , text){
+    console.log (text.replaceAll(char , ""));
+}
+
+charRemove ("p" , "pontus")
+
+
+        
+
+
+
+
+
+
+      
+
+
