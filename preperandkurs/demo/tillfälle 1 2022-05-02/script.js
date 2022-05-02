dc42447461cfbcc437bc18ebe03cb63f51835840
Alrik He <alrik_he@hotmail.com>
2022-05-02 17:58:39 +0200

    tillfälle 1 med Rebeca , Ismail Geta

---
+++ preperandkurs/demo/tillfälle 1 2022-05-02/script.js
@@ -0,0 +1,44 @@
+/* hur man kommenterar!!
+alert("HEJ!!!");
+document.write("tjena");
+console.log("hello");
+*/
+
+// console.log("hello");   console.log("hello");
+
+
+// Variabler är behållare för värden 
+// har ett namn 
+// syntax
+
+let foo;  // skapat en variabel med namnet foo
+            //deklarera variabeln foo
+
+let bar = "333";
+
+foo = 999;  // ni lägger in ett värde till variabeln foo
+            // tilldela variabeln foo med värdet 888
+
+foo = 'let'; // text har altid " " eller ' '
+
+foo = 5 + 5; // lägger in nummer med matematic
+            //tilldelning med ett matematikt uttryck    
+
+foo = 2 * (3 + 3) ; 
+
+
+        // concatination
+foo = "hello" + 1;
+foo = 2 + " world";
+
+foo = "hej" + 1 + 1; // hej11
+foo = 1 + 1 +"hej";  // 2hej
+
+foo = 2;
+
+    // referera / använder
+
+console.log( foo + bar );
+
+let saldo = 2000;
+
