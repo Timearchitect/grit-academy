a5ab6b8eb2fb467bfcf17aeebed34ed8c6503c63
Alrik He <alrik_he@hotmail.com>
2022-05-05 17:50:40 +0200

    tillfälle 1 05 -05

---
+++ preperandkurs/demo/tillfälle 1 2022-05-05/script.js
@@ -0,0 +1,48 @@
+/*
+alert("hello");
+document.write("hej");
+console.log("tjenare");
+*/
+
+// Kommentarer är bra
+// Variabler
+// container som innerhåller data info
+// String number 
+
+let foo;  // deklarera en variabel
+let bar = "hello"; // deklaration med tilldelning
+
+foo = 5;  // tilldela en variabel
+
+foo = 1 + 1 ; 
+
+// concatination
+ // text + en annan text eller siffror
+
+let result = 100;
+
+//result = result + 1;
+//result += 1;
+//result ++; 
+
+console.log(result,8);
+
+
+// Datatyper
+
+        //boolean
+foo = true;   // true , sant  1 ON
+foo = false;  // false,falskt 0 OFf
+
+        // number
+foo = 5;     // Integer , heltal         
+foo = 0.1;   // Float   , decimaltal
+
+        // String
+foo = "hello";  // String, sträng
+foo = '$';      // character, karaktär
+
+
+
+
+
