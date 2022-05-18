31b4d9eece814166c50707f587468c4f3a85a079
SamosTheWiseSage <104500429+SamosTheWiseSage@users.noreply.github.com>
2022-05-18 09:38:00 +0200

    Add files via upload

---
+++ preperandkurs/assignments/uppgift 1.5 GE/script.js
@@ -0,0 +1,27 @@
+let age=17; 
+let vip;
+function ShowMeID(){
+    var input = document.getElementById('Age');
+    input.value;
+if(document.getElementById('Age').value >= 18)
+    alert("Access granted");
+else if(document.getElementById('VIP').checked)  
+alert("Welcome in my best VIP costumer :)"); 
+ else if(document.getElementById('Age').value <= 16)
+    alert("Access denied")
+}
+//if (age > 17 == true ) {
+  //  alert = "Good day";
+ // } else {
+   // alert = "Good evening";
+ // }
+  //document.getElementById("Response").innerHTML = alert;
+  /* perfect code preserve 
+      var input = document.getElementById('Age');
+    input.value;
+if(document.getElementById('Age').value >= 18)
+    alert("YES 1");
+else if(document.getElementById('VIP').checked)  
+alert("welcome my best costumer"); 
+ else if(document.getElementById('Age').value <= 16)
+    alert("No 2") */
