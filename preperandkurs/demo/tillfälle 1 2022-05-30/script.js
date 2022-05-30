fe66d80190aaeefb8ec2ee56320fbbece52e749c
Alrik He <alrik_he@hotmail.com>
2022-05-30 16:09:47 +0200

    demo

---
+++ preperandkurs/demo/tillfälle 1 2022-05-30/script.js
@@ -0,0 +1,54 @@
+//alert("HELLO");
+/*   JAVASCRIPT TILLFÄLLE 1 ,2022-05-30
+    document.write("HEJ");
+    console.log("TJENARE!!!");
+*/
+
+// Variabler 
+
+
+// Deklarera en variabel som heter foo
+let foo;
+
+// foo tilldelas med siffran 5 
+foo = 23;
+
+// bar deklareras + tilldelas med 5
+let bar ;
+
+bar = "hello"; // skriver över 5 med "hello"
+
+console.log(bar );
+
+
+//För att öka en variabel med siffror
+// olika sätt
+foo = foo + 1;
+foo += 2;
+foo ++;
+
+
+
+
+// DATATYPER
+// värde = allt som kan vara i en variabel 
+// värdet är innehållet i en variabel
+// om det sitter i en variabel etc värden: string siffror object arrayer undefiend symbol
+
+
+
+//           String
+bar = "Texter" // String , strängar
+bar = '@'   // char, karaktär
+
+//         Number
+bar = 5     //integer,  heltal
+bar = 0.5   //float ,  decimaltal
+
+//           Boolean
+bar = true  // boolean , boolean true eller false
+            // kan tolkas som 1 eller 0
+
+
+            
+
