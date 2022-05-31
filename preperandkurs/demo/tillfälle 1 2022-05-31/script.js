d12eb87dd3e15e49cbd325a4ca13aa948faf4f2c
Alrik He <alrik_he@hotmail.com>
2022-05-31 18:51:02 +0200

    demo 2022-05-31

---
+++ preperandkurs/demo/tillfälle 1 2022-05-31/script.js
@@ -0,0 +1,51 @@
+
+/*
+alert("hello");
+document.write("HEJ!!");
+console.log("Tjenare!!");
+*/
+ // 1) Variabler
+
+ // deklarera variabeln foo
+let foo;
+
+ // tilldela värdet 5 till foo
+
+foo = "hello"; 
+// deklarering + tilldelning
+let bar = 5;
+foo = foo + " world";  // konkatinerar
+
+foo = 5 ; 
+foo = foo + 5;
+
+console.log(  "2hej" );
+
+foo =  "hello";
+
+console.log( bar +1 +1 + foo  );
+
+foo = 5;
+
+foo = foo + 2;  // ändra utan att skriva över
+foo += 2;    // förkortning
+foo ++;     // ökar med 1 
+
+
+// Datatyper
+
+let alrik = true;
+
+//          String
+alrik = "hello";    // String  , sträng
+alrik = '@';        // char , karaktär  
+
+//           Number
+alrik = 10;         // integer, Heltal
+alrik = 0.5;        // float , Decimaltal 
+
+//          boolean
+alrik = true;       // boolean , boolean kan vara 1
+alrik = false;       // boolean , boolean kan vara 0
+
+
