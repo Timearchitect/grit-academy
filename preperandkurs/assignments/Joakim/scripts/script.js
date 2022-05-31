b7eaed1c3dbcb821f747d6bce8c7e16708bf5c2e
Jocke878 <94178793+Jocke878@users.noreply.github.com>
2022-05-31 19:34:23 +0200

    Add files via upload

---
+++ preperandkurs/assignments/Joakim/scripts/script.js
@@ -0,0 +1,19 @@
+var message="hello";
+
+var foo=0; 
+var bar=0;
+
+
+
+alert(message);
+document.write("hej")
+console.log(foo/bar);
+if (foo/bar) {
+    //  block of code to be executed if the condition is true
+  }else{
+alert("you can't divide us")
+  }
+
+function test() {
+    console.log("divide")
+}
