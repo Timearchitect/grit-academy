9521f37c85a6e15863d79cf25f3ed9d2808942da
Alrik He <alrik_he@hotmail.com>
2022-06-22 18:11:09 +0200

    demo 2

---
+++ preperandkurs/demo/tillfälle 2 2022-06-22/script.js
@@ -0,0 +1,55 @@
+'use strict';
+// arrayer variablar men flera
+
+// variabel
+let foo; //dekaration
+foo = 5; //tilldelning
+foo = 888;
+
+let bar; //dekaration
+
+//index  
+bar =   [5, "hej", 999, true]; //tilldelning
+
+// 5 är ett element
+// "hej" är ett element
+// osv  (3 står för längden av element)
+
+bar[0] = bar[2]+1;
+
+console.log(bar.length)
+
+bar.push(55555); // lägger till på slutet
+bar.pop();
+console.log(bar.join());
+
+let que = ["Sara","Arzije","Franz-Josef","Xhulian"];
+
+// Datatyper
+let x;
+
+// value, värde  
+//   1)  String
+x = 'Hello' // String , Sträng 
+x = "$"     // char , karaktär
+
+
+//   2)   Number
+x = 5;      // Integer  , Heltal   
+x = 0.1;    // Float    , Decimaltal
+
+//   3)   Boolean
+x = false    // Boolean , Boolean
+
+// Object , Undefined , Symbol ,null
+
+
+
+
+
+
+
+
+
+
+
