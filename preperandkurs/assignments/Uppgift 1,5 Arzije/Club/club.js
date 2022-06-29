1bb909961846b218d8c31296b9eeba6d0941653c
Arzije <107810597+Arzije@users.noreply.github.com>
2022-06-29 21:11:49 +0200

    Uppgift 1,5

---
+++ preperandkurs/assignments/Uppgift 1,5 Arzije/Club/club.js
@@ -0,0 +1,18 @@
+let vip;
+if (vip)
+    console.log("Welcome in my best VIP customer")
+
+
+
+let age = 17;
+if (age < 17){
+    console.log("Access granted"); 
+    //alert("Access granted")
+    
+    if (vip)
+    console.log("Welcome in my best VIP customer")
+}
+
+else
+    console.log("Access denied");
+    //alert("Access denied")
