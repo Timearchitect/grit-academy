bb71624ddfa609a6d1f156d8b439fb64fb6180c8
Arzije <107810597+Arzije@users.noreply.github.com>
2022-07-03 18:05:30 +0200

    Uppgift 2 Arzije

---
+++ preperandkurs/assignments/Uppgift 2 Arzije/script.js
@@ -0,0 +1,42 @@
+
+
+                // Här har jag kvar lite av vad jag testat som jag kommenterat bort
+// Assignment 2c
+
+ /*
+ let citynames = ["kiruna", "uppsala" , "malmo", "goteborg", "stockholm"]; 
+  for ( let element of citynames){
+
+    console.log(element)
+ }
+ */
+
+
+ /*
+ let str = "kiruna"; {
+
+    console.log(str [0]);
+ }
+ */
+
+
+ /*let citynames = ["kiruna", "uppsala" , "malmo", "goteborg", "stockholm"];{
+
+    for ( let element of citynames);
+    console.log("kiruna" [0]);
+    console.log("kiruna" [1]);
+    console.log("kiruna" [2]);
+    console.log("kiruna" [3]);
+    console.log("kiruna" [4]);
+    console.log("kiruna" [6]);
+    
+ }
+*/
+
+let enStad = "kirunauppsalamalmogoteborgstockholm";{
+
+    for (let i=0; i< enStad.length; i++)
+    console.log(enStad[i]);
+}
+
+
