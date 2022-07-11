b9057cea92419d4ba78db4efecd0569a36c39606
Alrik He <alrik_he@hotmail.com>
2022-07-11 19:03:53 +0200

    tillfälle1  2022-07-11

---
+++ preperandkurs/demo/tillfälle 1 2022-07-11/script.js
@@ -0,0 +1,49 @@
+/*
+document.write("HEJ");
+alert("Hello");
+
+*/
+//console.log("TJENARE!!!");
+//console.log("TJENARE!!!");console.log("TJENARE!!!");
+//console.log("TJENARE!!!");
+
+
+// Variabler är en behållare med ett namn
+
+//syntax för att skapa en variabel som heter foo
+//Deklarera variabeln foo
+let alrik;
+//Tilldela värdet 5 till variabeln foo
+alrik = 5;
+
+alrik = "Hello";
+
+
+// när man adderar text med något så blir resultatet alltid en text
+console.log( "hello" + (1 + 1));  
+console.log( 1 + 1 + alrik );  
+
+//deklarera samt tilldela i samma rad
+let foo = 85;
+// innehåll <--- tilldelar  
+foo = foo  + 1 ;
+foo = foo  + 1 ;
+foo = foo  + 1 ;
+foo = foo  + 1 ;
+foo = foo  + 1 ;
+
+foo += 1;   // förkortning
+
+foo ++;   // ökar med 1
+
+
+
+let bar = "hej";
+
+bar = bar  + "a" ;
+bar = bar  + "a" ;
+bar = bar  + "a" ;
+bar = bar  + "a" ;
+bar = bar  + "a" ;
+
+bar += "T";   // förkortning
