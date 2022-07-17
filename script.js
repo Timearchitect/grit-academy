393c4bb4c75b74feab3117bc1420191739cddbc6
Nermin53 <109095361+Nermin53@users.noreply.github.com>
2022-07-17 21:32:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
++let
+if (age >=17) {
+    console.log("granted")
+    console.log("denied")
+    
+}
+
+if (vip)true
+console.log("acess granted")
+
+
+"else"
+      console.log("denied")
+
+
+if (vip)false
+
+
+if ("Welcome in my best vip customer") {
+    
+}
