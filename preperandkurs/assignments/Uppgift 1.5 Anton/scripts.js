8d107fc02239968f61953211286dc5afbe75eee1
Mauritz85 <109244116+Mauritz85@users.noreply.github.com>
2022-07-20 22:46:41 +0200

    Add files via upload

--- preperandkurs/assignments/Uppgift 1.5 Anton/scripts.js
+++ preperandkurs/assignments/Uppgift 1.5 Anton/scripts.js
@@ -1,8 +1,17 @@
-let vip
-let age =17
-if (age>=18) {.alert {
-    padding: 20px;
-    background-color: #f44336; /* Red */
-    color: white;
-    margin-bottom: 15px;
+let checkValue = document.getElementById("vip")
+let inputValue = document.getElementById("ageinput")
+
+
+let vip = checkValue
+let age = inputValue
+
+if (vip=true) {alert("VIP Welcome");
+show_popup("Welcome in my best VIP customer");}
+
+else {
+    if (age>=18) {alert("Access granted");
+    show_popup("Access granted"); }
+    
+    else {alert("Access denied");
+    show_popup("Access denied");
   }}
