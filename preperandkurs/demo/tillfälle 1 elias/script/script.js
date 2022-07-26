469b5bdd8eb4bae498f9a247828b5ce19fe5b77c
Alrik He <alrik_he@hotmail.com>
2022-07-26 17:19:26 +0200

    demo1 elias

---
+++ preperandkurs/demo/tillfälle 1 elias/script/script.js
@@ -0,0 +1,24 @@
+/*
+alert("hello");
+document.write("Hej!");*/
+
+
+//1)  Variabel
+
+// skapa en variabel som heter foo
+// deklarera/declare en variabel
+let x;
+
+// lägg in siffran 5 i variabeln foo
+// tilldela/assign value 5 to foo
+x = 5;
+
+//Deklarera + tilldela
+let elias = 1337;
+
+x = 888;
+x = "hello";
+console.log(x);
+console.log(1+1);
+
+
