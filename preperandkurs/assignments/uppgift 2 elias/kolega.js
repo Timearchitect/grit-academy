552b73285d5a59f0f55093c818fc953b2a66a143
Kolegaaa <elias_king_9@hotmail.com>
2022-08-01 17:04:49 +0200

    uppgift 1,5

---
+++ preperandkurs/assignments/uppgift 2 elias/kolega.js
@@ -0,0 +1,22 @@
+
+const vip=true 
+const age=18
+
+
+if ( vip) {
+    console.log ("WELCOME MY BEST CUSTOMER ;)");
+    
+  }
+    else
+    if (age >= 18) {
+     
+      console.log ("Access granted");
+      
+    }
+      else
+      {
+      console.log ("Access denied");
+}
+    {
+    console.log ("NOO");
+    }
