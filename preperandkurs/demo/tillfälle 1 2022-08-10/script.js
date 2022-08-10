66af1c24b168e1d1f1aa80ade633fc5a5db3ee37
Alrik He <alrik_he@hotmail.com>
2022-08-10 19:02:11 +0200

    uppgift 1 alrik

---
+++ preperandkurs/demo/tillfälle 1 2022-08-10/script.js
@@ -0,0 +1,41 @@
+/*
+alert(888);
+document.write(999);
+console.log(111);
+*/
+
+
+// Varibler
+// Skapa en variabel
+// let som är ett nyckel ord sedan ett namn
+let foo; //deklarera en variabel med namnet foo
+
+// lägger in ett värde i variabeln foo
+foo = 5;  //tilldela varabeln foo med nummer 5
+foo = "hej";  //tilldela varabeln foo med nummer 5
+
+
+let bar = 8; // deklarerar bar & tilldelar den med 8
+
+
+
+console.log("hej" + (8 + 1) );
+
+
+let money = 0 ;
+console.log("My wallet contains: " + money + " kr" );
+
+console.log(`My wallet contains: ${ money } kr`);
+
+// Array
+// flera variabler i ett
+
+let array; // deklarerar en variabel
+//index   0 ,  1    ,  2    
+array = [ 5 , "HELLO" , 888]; // tilldelar vi en array
+
+array [0] = array [2]
+
+array.push("tjena")
+array.pop();
+console.log(array.length);
