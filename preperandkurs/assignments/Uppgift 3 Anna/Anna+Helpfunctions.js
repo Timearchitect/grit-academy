04d9e3b616cf25af084ba2f80be559778cf1ddfd
Anna Sundberg <annasundberg95@gmail.com>
2022-08-15 16:52:24 +0200

    Uppgift 3

---
+++ preperandkurs/assignments/Uppgift 3 Anna/Anna+Helpfunctions.js
@@ -0,0 +1,11 @@
+function charRemove () {
+    
+    originalString = 'oscar also wants an orange';
+    newString = originalString.replace(/o/g, '');
+    
+console.log(newString)
+
+}
+
+
+
---
+++ preperandkurs/assignments/Uppgift 3 Anna/object.js
@@ -0,0 +1,9 @@
+let cat= {
+    NameOfCat: "Gustav", alive:true 
+    
+}
+    
+    
+function myFunction() {
+    alert("The cats name is Gustav!");
+  }
