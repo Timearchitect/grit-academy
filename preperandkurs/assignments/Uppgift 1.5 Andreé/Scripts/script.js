96011f54e5ed650277e939e4c3b9ab5ac29a2b35
Raq-3773 <110932182+Raq-3773@users.noreply.github.com>
2022-08-17 12:06:41 +0200

    Uppgift 1.5 Andreé

---
+++ preperandkurs/assignments/Uppgift 1.5 Andreé/Scripts/script.js
@@ -0,0 +1,27 @@
+
+
+//button 
+/*
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", function(e) {
+
+let age = document.getElementById("numberBox").value;
+console.log(age);
+*/
+
+function returnNumber() {
+    let age = document.getElementById("numberBox").value;
+    console.log(age);
+
+    let vip = document.getElementById("checkBox").value;
+    console.log(vip)
+
+   if (vip === true) {
+    alert("Welcome in my best VIP customer :)");
+} else if (age >= 18) {
+    alert("Access granted"); 
+}  else {
+    alert("Access denied");
+};
+};
