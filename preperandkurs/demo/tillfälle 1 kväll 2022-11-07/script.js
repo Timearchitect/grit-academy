53b88eeabb5b9c84ecbffab8b71557a0c2a4e1ae
Alrik He <alrik_he@hotmail.com>
2022-11-07 18:31:22 +0100

    tillfälle 1 2022-11-07
    demo

---
+++ preperandkurs/demo/tillfälle 1 kväll 2022-11-07/script.js
@@ -0,0 +1,37 @@
+/*
+   <---- fin kod ...bättre om ni skrev blablbala
+dåligt 
+    |
+    v
+   alert( "tjena" );
+
+*/
+//document.write( "hejsan" );
+console.log(  "YO!!!!"  );
+
+//variabler är some boxar med namn
+//deklaration av variabel= skapa en variabel
+
+let alrik = "sdfsfdgsdgs"
+
+// tilldela et annat värde på variabeln alrik
+
+alrik = 50
+
+
+alrik = "hello"
+
+
+
+//för att använda en variabel skriv namnet
+
+console.log(  alrik + alrik );
+
+
+let money = 0
+
+money ++
+money += 1
+money = money + 1
+
+console.log(  money );
