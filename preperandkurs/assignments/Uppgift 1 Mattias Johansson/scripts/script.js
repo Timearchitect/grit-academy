1e927f84bd64d70df5835bf5b86666a0244d43f5
Mattias9010 <mattias9010@hotmail.com>
2022-11-07 21:48:23 +0100

    Mattias9010
    Uppgift 1

---
+++ preperandkurs/assignments/Uppgift 1 Mattias Johansson/scripts/script.js
@@ -0,0 +1,16 @@
+alert("Hallå där! Sugen på att ut och resa? Då har du kommit helt rätt!");
+document.write("För mer info email: Mattias9010@hotmail.com");
+console.log("Hello world");
+
+//Variabler = box med info
+// deklarerar variabeln Mattias / Skapar boxen
+let mattias = 50
+
+
+console.log( mattias + mattias)
+mattias = "tjenare" //tilldela
+console.log( mattias + "mattias")
+
+let foo = 0
+let bar = 0
+console.log( foo / bar )
