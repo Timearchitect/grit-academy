94a4e2a0224513b30653b5c38c3f8594e0745a8a
Alrik He <alrik_he@hotmail.com>
2022-11-07 12:05:09 +0100

    tillfälle 1
    2022-11-07

---
+++ preperandkurs/demo/tillfälle 1 2022-11-07/script/script.js
@@ -0,0 +1,17 @@
+//alert("TJENA!!!!");
+//document.write("HEJSAN");
+
+/* ...block kommentar
+console.log("Hello world");
+*/
+
+// Variabler är boxar som innehåller information
+
+// deklarerar variabeln foo / skapar boxen foo
+//let foo = (50 + 50) * 2  man kan ha uttryck
+
+let foo = 50 // deklarerar
+console.log( foo + foo )
+
+foo = "tjenare" //tilldela
+console.log( foo + "foo" )
