754bb603df5c8e5a3ccf73f0b59cdbe8cd5bc6ae
Ceeessan <Cessan__94@hotmail.com>
2022-11-09 20:27:03 +0100

    Tillfälle 1
    2022-11-07 Cecilia

---
+++ preperandkurs/demo/Tillfälle 1 2022-11-07 Cecilia/Scripts/script.js
@@ -0,0 +1,21 @@
+/*alert("Hallihallå");
+document.write("Error");
+*/
+/*
+console.log("Hello world")
+*/
+
+let foo = 50
+foo=0
+console.log(foo)
+let bar = 50
+bar = 0
+console.log(bar)
+
+console.log(foo/bar)
+
+if (foo/bar>=0){
+console.log ("\nYou can't divide us!\n");
+}else{
+    console.log("\nYou can't divide us!\n")
+}
