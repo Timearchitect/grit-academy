d340fde1aefd80bd4ba021f06619e69a7a5060e9
annelisandstom <117845700+annelisandstom@users.noreply.github.com>
2022-11-09 17:49:03 +0100

    Uppgift 1 Anneli

---
+++ preperandkurs/assignments/Uppgift 1 Anneli/script/script.js
@@ -0,0 +1,13 @@
+alert("You cant divide us")
+
+console.log("Foo");
+console.log("bar");
+let foo = 0;
+let bar = 0;
+let result = foo / bar;
+
+console.log("resultat:");
+console.log(foo / bar);
+function myfunction() { 
+    console.log("divide")
+}
