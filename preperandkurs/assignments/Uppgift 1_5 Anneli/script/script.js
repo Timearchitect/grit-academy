0dd94d1b4fd72c32e56c208257df3226fa14f1d0
annelisandstom <117845700+annelisandstom@users.noreply.github.com>
2022-11-12 18:14:37 +0100

    Uppgift 1_5 Anneli

---
+++ preperandkurs/assignments/Uppgift 1_5 Anneli/script/script.js
@@ -0,0 +1,19 @@
+function check() {
+
+
+   
+    let age = document.getElementById("inputAge").value;
+    console.log(age);
+    let vip = document.getElementById("checkbox").checked;
+    console.log(vip);
+
+    if (vip) {
+        alert("Welcome my best VIP customer :)");
+    }   
+    else if  (age >=18) {
+        alert("Access granted");
+    }
+    else {
+        alert("Access denied");
+    }
+}
