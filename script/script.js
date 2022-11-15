08f86fc223427c57e8994eda07570d31b0f26712
arbresha98 <117952409+arbresha98@users.noreply.github.com>
2022-11-15 02:22:08 +0100

    uppgift 1 arbresha

---
+++ script/script.js
@@ -0,0 +1,14 @@
+//alert("Hej!!");
+document.write("välkommen");
+console.log("hello world");
+
+// variablar är boxar som innehåller information
+
+// deklarerar variabeln foo / skapar boxen 
+let foo = 30
+console.log( foo + foo )
+foo = ( 456 )  
+
+foo = "hallo"
+
+console.log( foo )
