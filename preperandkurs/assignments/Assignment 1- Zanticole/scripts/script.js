db4cd9bccb357144670ab75ad90e8d81897a45e2
Alrik He <alrik_he@hotmail.com>
2022-11-21 16:33:21 +0100

    sorting

---
+++ preperandkurs/assignments/Assignment 1- Zanticole/scripts/script.js
@@ -0,0 +1,17 @@
+//alert("You cant divide us!");
+document.write("Kitty world");
+
+
+
+let foo = 0;
+let bar = 0;
+
+console.log ( foo );
+console.log ( bar );
+
+console.log ( foo / bar );
+
+//osäker på hur jag ska skapa en alert när den inte producerat ett nummer
+if ( foo / bar ) { alert("You can't divide us!"); }
+if (Number === "0") {
+    text = "You cant divide us!";}
