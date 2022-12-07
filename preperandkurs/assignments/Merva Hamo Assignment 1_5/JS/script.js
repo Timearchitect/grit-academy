5110c00a231ffdf250041bf607688d7c30fd9fb6
MervaHamo <119268778+MervaHamo@users.noreply.github.com>
2022-12-07 20:21:59 +0100

    Merva Hamo Assignment 1_5
    Preparandkurs 2022/12/07

---
+++ preperandkurs/assignments/Merva Hamo Assignment 1_5/JS/script.js
@@ -0,0 +1,15 @@
+let age = 17;
+let vip = true;
+
+if (age < 18 && vip == true) {
+    console.log ("Welcome in my best vip customer:");
+
+}
+else if (age >= 18 || vip == true) {
+    console.log ("Access granted")
+
+}
+else {
+    console.log ("Access denied");
+
+}
