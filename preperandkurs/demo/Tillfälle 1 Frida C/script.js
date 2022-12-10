6b6804b3b2d4f9ef2107a5dd66d00d2de222a3d9
fridachristiansen <fridach03@gmail.com>
2022-12-10 10:20:06 +0100

    Tillfälle 1 10/12 2022

---
+++ preperandkurs/demo/Tillfälle 1 Frida C/script.js
@@ -0,0 +1,17 @@
+
+
+document.write(  "hello" );
+
+console.log( "1!!" );
+
+console.log( "2!!");
+
+let foo;    // deklarera skapa en variabel som heter foo
+foo = "HEJ"   // tilldela värdet 888 till foo
+
+let alrik = 555//deklarera & tilldela
+alrik = 111
+
+console.log(  foo + alrik );
+
+
