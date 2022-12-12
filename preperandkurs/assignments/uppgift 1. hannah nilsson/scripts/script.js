99d15b17c057a881898132f98c7dacdf309dabdc
Hannis1 <120170612+Hannis1@users.noreply.github.com>
2022-12-12 12:18:52 +0100

    uppgift 1 hannah

---
+++ preperandkurs/assignments/uppgift 1. hannah nilsson/scripts/script.js
@@ -0,0 +1,10 @@
+let foo // deklarera skapa en veriabel som heter foo 
+foo = o // tilldela värdet 0 till foo
+
+let bar // deklarera skapa en veriabel som heter bar
+bar = 0 // tilldela värdet 0 till bar
+
+console.log(  foo  )
+console.log(  bar ) 
+
+alert ( "you cant divide us!" )
