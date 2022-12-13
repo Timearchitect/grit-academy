3029bd3a7c141b8b66781d52a4e7197a891ecee1
Hannis1 <120170612+Hannis1@users.noreply.github.com>
2022-12-13 23:22:54 +0100

    Add files via upload

---
+++ preperandkurs/assignments/uppgift 1_5 hannah nilsson/script.js
@@ -0,0 +1,14 @@
+let age = 17;
+let vip = true;
+
+if (age< 18 && vip == true) {
+   console.log ("Welcome in my best vip customer:");
+
+}
+else if (age <= 18 vip == true) {
+    console.log ("Access granted")
+
+}
+else {
+     console.log ("Access denied");
+}
