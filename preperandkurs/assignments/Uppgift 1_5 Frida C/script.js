5bb681f1682786dc71ddb08e87eb8229e14e76ed
fridachristiansen <fridach03@gmail.com>
2022-12-13 20:15:31 +0100

    Uppgift 1_5 Frida C

---
+++ preperandkurs/assignments/Uppgift 1_5 Frida C/script.js
@@ -0,0 +1,21 @@
+let vip;
+
+vip = true
+let age; 
+
+age=17
+
+if (age >= 18)
+{
+    alert("Access granted");
+}
+else 
+(age <= 18) 
+{
+    alert ("Access denied");
+}
+
+if (vip == true) {
+    console.log ("Welcom in my best VIP costumer:)")
+}
+
