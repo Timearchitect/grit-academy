13be59e18d01eb7affedce38eb9f82736868f2cc
Alrik He <alrik_he@hotmail.com>
2022-12-14 10:17:10 +0100

    feedback frida

--- preperandkurs/assignments/Uppgift 1_5 Frida C/script.js
+++ preperandkurs/assignments/Uppgift 1_5 Frida C/script.js
@@ -19,3 +19,22 @@ if (vip == true) {
     console.log ("Welcom in my best VIP costumer:)")
 }
 
+
+
+
+/*
+
+let vip = true
+let age =17
+
+if (vip == true) 
+    console.log ("Welcome in my best VIP costumer:)")
+else
+
+    if (age >= 18)
+        alert("Access granted");
+    else 
+        alert ("Access denied");
+
+
+*/
