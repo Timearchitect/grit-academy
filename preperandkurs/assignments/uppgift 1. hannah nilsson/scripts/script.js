b9b3cc02cffdde271474e6d4aeab39375e0c7b36
Alrik He <alrik_he@hotmail.com>
2022-12-19 19:08:52 +0100

    demo cleaning

--- preperandkurs/assignments/uppgift 1. hannah nilsson/scripts/script.js
+++
@@ -1,9 +0,0 @@
-let foo // deklarera skapa en veriabel som heter foo 
-foo = 0 // tilldela värdet 0 till foo
-
-let bar // deklarera skapa en veriabel som heter bar
-bar = 0 // tilldela värdet 0 till bar
-
-console.log(  foo/bar  )
-
-alert ( "you cant divide us!" )
