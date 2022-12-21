dda7730e4da2b76ef6ecc401f9bad124a6690ad5
Alrik He <alrik_he@hotmail.com>
2022-12-21 19:05:14 +0100

    asdas

---
+++ preperandkurs/assignments/tillfälle 1 2022-12-19/script.js
@@ -0,0 +1,60 @@
+/*
+alert( 888 );
+
+document.write( "hejsan" );
+
+console.log( "TJENARE!!!" );
+*/
+
+
+
+
+//    (1) Varibler 
+
+let foo    // declare/deklarera en variabel som heter foo
+foo = 888  // assign/tilldela variabeln foo med siffran 888
+
+foo = "hej" + "san"
+
+foo = 1 + 1
+
+foo = "hej" + (1 + 1) 
+
+console.log( foo + foo) // när man printar ut en variabel så visa innehållet bara
+
+foo = 0
+
+
+foo = foo + 1 // öka foo med 1
+
+foo += 1 // öka foo med 1
+
+foo ++ // öka foo altid med 1
+
+
+// datatyper
+
+let x = "hej";
+
+//string
+x = "hej" // string/sträng           Str
+x = 'c'   // character/karaktär       char 
+
+// number
+x = 5     // heltal/integer           int 
+x = 0.5   // float/double/decimaltal float/ f  
+
+// boolean
+x = true   // true/sant   boolean     bool
+x = false  // false/falsk boolean    bool
+
+// string + nummer = string
+// string + boolean = string
+// string + string = string
+
+// nummer + boolean = nummer
+// nummer + nummer = nummer
+
+// boolean + boolean = nummer
+
+
