09b1501e322b3664c22879a5b9335b4b1319c588
Simon Andersson <108905796+Grymto@users.noreply.github.com>
2022-12-26 22:01:17 +0100

    Add files via upload

---
+++ preperandkurs/assignments/Assaignment 1_5 Simon/script/script.js
@@ -0,0 +1,25 @@
+let age = 17;
+let vip = false;
+
+alert("Assignment 1_5");
+
+console.log("Club Exlusive");
+
+function ageClick() {
+  age = parseInt(document.getElementById("ageInput").value, 10);
+  vip = document.getElementById("vipCheckbox").checked;
+
+  if (vip == true) {
+    console.log("Welcome in my best VIP customer!");
+    alert("Access Granted!");
+  } else if (age >= 130) {
+    console.log("Run! Undead is Here!");
+    alert("Undead is here!");
+  } else if (age >= 18) {
+    console.log("Welcome!");
+    alert("Access Granted!");
+  } else {
+    console.log("you are under age!");
+    alert("Access Denied!");
+  }
+}
