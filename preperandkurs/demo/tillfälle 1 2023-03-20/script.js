37d44807d727ae8600ea77c7d367ae36f0bbddee
Alrik He <alrik.he@gritacademy.se>
2023-03-20 12:01:37 +0100

    tillfälle 1

---
+++ preperandkurs/demo/tillfälle 1 2023-03-20/script.js
@@ -0,0 +1,32 @@
+/*  
+alert("HEJ")  
+document.write("Tjenare!!!")
+*/
+
+console.log("hej") // skriver ut hej på konsolen
+
+// variabler
+
+let foo       // deklarera  
+foo = "hej"   // tilldela
+foo = "1"     // tilldela skriver över
+console.log( foo ) // skriver ut hej på konsolen
+
+let bar = 1  // deklarerat & tilldelat bar med 999
+
+console.log( (2 + 2) * 2 ) //pemdas
+console.log(  "hej" + "san" + (2 + 2)  );
+
+console.log( foo + bar );
+
+//öka med en siffra teniker
+bar = bar + 1  // öka med 1 oasett bars värde
+bar += 1 // short cut
+bar ++ // öka med 1
+
+console.log(bar);
+
+//öka med texter
+foo = foo + "hej"
+
+console.log(foo);
