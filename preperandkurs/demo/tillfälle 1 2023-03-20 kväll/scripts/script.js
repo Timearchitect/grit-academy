31083efa3168d3660bf29139963e3bcf7027a708
Alrik He <alrik.he@gritacademy.se>
2023-03-20 21:13:00 +0100

    stuff

---
+++ preperandkurs/demo/tillfälle 1 2023-03-20 kväll/scripts/script.js
@@ -0,0 +1,25 @@
+
+/*alert("hej")
+document.write("Tjenare")
+console.log("Hello world")
+*/
+
+
+// variablar 1)
+
+let x   // deklarera en variabel som heter alrik
+x = 5   // tilldela ett värde x med 5
+x = 1   // skriver över värdet 9 med 1
+let bar = 9 //deklarering & tilldelning på samma rad
+bar = "hej" 
+
+console.log(bar)
+
+console.log( 1 + 1 )
+console.log( "hej" + "san" )
+console.log( "1" + "1")
+
+console.log( (2 + 3) * 2 )
+console.log( bar + x )
+
+
