6069675afb7259de6d67bbee17804630ae714a87
Pedram3 <128402216+Pedram3@users.noreply.github.com>
2023-03-26 21:51:17 +0200

    Add files via upload
    Pedram Abedini uppgift 1.5

---
+++ preperandkurs/assignments/Pedram Abedini uppgift 1.5/script.js
@@ -0,0 +1,14 @@
+function checkAge(age, vip) {
+    if (vip) {
+      return true;
+    } else {
+      if (age >= 18) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+  
+
+  
