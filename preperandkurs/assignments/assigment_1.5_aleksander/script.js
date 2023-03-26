0023c00805f9fec0910183bc948f1721fef5c947
AleksanderBjelk <bjelkaleksander@gmail.com>
2023-03-26 21:07:04 +0200

    aleksander assignment 1_5

---
+++ preperandkurs/assignments/assigment_1.5_aleksander/script.js
@@ -0,0 +1,17 @@
+let vip = true;
+//let age = 17
+function returnText() {}
+let input = document.getElementById("enterBtn").value;
+let x = document.getElementById("cbox").value("mittvalue");
+document.getElementById("vip").innerHTML = x;
+
+if (vip == true) {
+    alert("Welcome in my best customer! :)");
+} else if (input >= 18) {
+    alert("Access granted");
+} else {
+    alert("Acess denied");
+}
+
+if (input >= 18 == true) {
+}
