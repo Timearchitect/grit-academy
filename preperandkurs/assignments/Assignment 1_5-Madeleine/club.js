1b438d141e5b0b09692e34534a9d83f307c989cc
madeleinefrank <127849239+madeleinefrank@users.noreply.github.com>
2023-03-27 09:34:01 +0200

    Uppgift 1_5 Madeleine

---
+++ preperandkurs/assignments/Assignment 1_5-Madeleine/club.js
@@ -0,0 +1,15 @@
+let age = 17;
+let vip = age >=18
+
+if (vip){
+    alert ("Access granted");
+    console.log("True age is over 18")
+}
+else {
+    alert ("Access denied");
+}
+
+ vip = true
+if (vip){
+    console.log("Welcome in my best customer");
+}
