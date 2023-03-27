a55b120cdc405a358ee2e02eb55042faff0ebaaa
linnea-ea <128360461+linnea-ea@users.noreply.github.com>
2023-03-27 12:33:40 +0200

    Inlämning test

---
+++ preperandkurs/assignments/Uppgift 2 - The club/assignment 1 + linnea ferman alvsten/script/script.js
@@ -0,0 +1,40 @@
+
+/*
+let age = 16
+let vip = false
+
+    if(vip)
+    {
+        console.log("Access granted");
+        alert ("Access granted")
+    } else {
+        if (age >= 18) {
+            console.log("Access granted");
+            alert ("Acces granted")
+        } else {
+            console.log("Access denied");
+            alert ("Access denied")
+        }
+    }
+    */
+
+
+/*
+//let vip = true
+let age = 18
+
+if (age = 18) {
+    console.log("Access granted");
+    //alert ("Access granted")
+} else {
+    console.log("Access denied");
+    //alert ("Access denied")
+}
+*/
+
+/*
+if (vip = true) {
+    console.log(Welcome my best VIP customer);
+    //alert ("Welcome my best VIP customer")
+}
+*/
