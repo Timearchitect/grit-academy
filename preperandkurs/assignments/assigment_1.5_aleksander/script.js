9fc2e8be83762946ea427aad73839220dbd49319
Alrik He <alrik.he@gritacademy.se>
2023-03-27 10:19:58 +0200

    feedback

--- preperandkurs/assignments/assigment_1.5_aleksander/script.js
+++ preperandkurs/assignments/assigment_1.5_aleksander/script.js
@@ -1,7 +1,32 @@
-let vip = true;
+
 //let age = 17
-function returnText() {}
-let input = document.getElementById("enterBtn").value;
+function returnText() {
+    let x = document.getElementById("cbox").checked
+    let input = parseInt(document.getElementsByTagName("input")[0].value)
+
+    //document.getElementById("vip").innerHTML = x;
+    console.log(input)
+    let vip = x;
+
+    if (vip == true) {
+        alert("Welcome in my best customer! :)");
+    } else if (input >= 18) {
+        alert("Access granted");
+    } else {
+        alert("Acess denied");
+    }
+    
+}
+
+
+
+let input = document.getElementById("enterBtn")
+input.addEventlistener( "onclick", ()=>{
+
+
+})
+
+
 let x = document.getElementById("cbox").value("mittvalue");
 document.getElementById("vip").innerHTML = x;
 
