b187d4bc244c937af17085b449b30a7e8eeda40f
AleksanderBjelk <bjelkaleksander@gmail.com>
2023-03-31 00:43:22 +0200

    aleksander_assignment3

---
+++ preperandkurs/assignments/assigment_3_aleksander/Aleksander+helpfunctions.js
@@ -0,0 +1,22 @@
+text= ["l", "hello"];
+
+for(let i = 0; i < text.length; i++)
+{
+    text[i] = text[i].replace('l', ' ');
+}
+console.log(text);
+
+
+function charRemove (l, hello){
+
+    let text2 = "l hello";
+
+    console.log(text2.replace('l', ' '));
+
+
+
+}
+
+
+
+
---
+++ preperandkurs/assignments/assigment_3_aleksander/script2.js
@@ -0,0 +1,13 @@
+let cat ={
+alive: true,
+alive: false,
+name: "Gustav",
+space: " ",
+pretext: "The cat name is:",
+eftertext: "and its alive"
+
+}
+function myFunction(){
+alert((cat.pretext) + (cat.space) + (cat.name) + (cat.space) + (cat.eftertext))
+
+}
