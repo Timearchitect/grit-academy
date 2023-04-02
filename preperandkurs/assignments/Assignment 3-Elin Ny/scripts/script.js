e56384056e1806e3275eb7da048e29a467c6addb
ElinNy <128368262+ElinNy@users.noreply.github.com>
2023-04-02 23:53:36 +0200

    Uppgift 3-Elin Ny
    Här kommer min uppgift 3.
    /Elin Ny

---
+++ preperandkurs/assignments/Assignment 3-Elin Ny/scripts/script.js
@@ -0,0 +1,10 @@
+function catBox() {
+    //Här skapar jag min funktion.
+    alert("katten heter " + cat.name); //Här skriver jag ut namn-egenskapen från objektet  i en alertbox.
+}
+
+let cat = {
+    //Här skapar jag mitt objekt
+    name: "Gustav", //Här är namnegenskapen
+    alive: true, //Här är levnadsstatus
+};
