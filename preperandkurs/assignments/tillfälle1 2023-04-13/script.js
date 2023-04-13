c20aa5abc90e53b39beb982a28ad2209d52b3050
Alrik He <alrik.he@gritacademy.se>
2023-04-13 12:49:28 +0200

    sdfsdfsdf

---
+++ preperandkurs/assignments/tillfälle1 2023-04-13/script.js
@@ -0,0 +1,18 @@
+/* 
+alert(888);  
+document.write(777);
+console.log(666);
+*/
+
+/*    1) variabler     */
+let foo  // skapar en variabel, deklarerar en varaibel foo
+foo = 5  // tilldela med nummer 5
+foo = "hej "// tilldelas med texten hej 
+
+let sala = "fotbollsspelare"  //deklarera + tilldela
+foo = sala
+//console.log( 1 + 1 );
+//console.log(  "hej"  +  "san"  );
+//console.log( 1 + 1 + "hej" );
+console.log( foo + sala );
+
