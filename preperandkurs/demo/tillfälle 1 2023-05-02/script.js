363d9a88da216ed58f840bcc359cd2a5a029e433
Alrik He <alrik_he@hotmail.com>
2023-05-02 11:57:03 +0200

    demo

---
+++ preperandkurs/demo/tillfälle 1 2023-05-02/script.js
@@ -0,0 +1,37 @@
+ /*
+ alert(500)
+ document.write(" <h1> hejsan </h1> ")
+ console.log(1111)
+*/
+
+// (1) Variabler
+// behållare som kan innehålla värden 
+
+// namn får ej ha mellanrum eller specialtecken , _ ok
+//  
+let money  //deklaration
+money = 5  // tilldelning av värdet 5
+
+money = money + 1;    
+
+console.log(money);
+
+let book = "hejsan" 
+book = book + " ! "
+console.log(book);
+
+console.log(1+1);
+console.log("hej" + "san" );
+console.log( "san" + (1 + 1) );
+console.log( 1 + 1 + "san" );
+
+console.log(money + book);
+console.log(6 + "hejsan ! ");
+
+
+// P = parentes
+// E = exponent
+// M = multiplikation
+// D = divition
+// A = addition
+// S = subtraktion
