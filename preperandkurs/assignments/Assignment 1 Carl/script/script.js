c1710d73b5e6e257d53d9b68eb842c6d85a2edaa
Carl Sundberg <34892891+Quaslusive@users.noreply.github.com>
2023-05-04 10:17:28 +0200

    Hemläxa

---
+++ preperandkurs/assignments/Assignment 1 Carl/script/script.js
@@ -0,0 +1,18 @@
+
+function displayDivideConsole() {
+    console.log("Divide");
+}
+
+function inputDivide() {
+
+    let foo = 0;
+    let bar = 0;
+
+    if (bar%foo !== 0) {
+        return alert("You cant divide us!");
+    
+   } else {
+console.log(foo/bar); 
+} 
+}
+    
