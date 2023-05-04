7572094574a9535ccd5219937fa7a2159a3ccda1
Alrik He <alrik_he@hotmail.com>
2023-05-04 10:08:30 +0200

    folder test

---
+++ preperandkurs/assignments/TILLFÄLLE1 KVÄLL/script/script.js
@@ -0,0 +1,20 @@
+// alert(555)
+// document.write(888)
+// console.log(9999)
+
+// 1) Variabler
+// behållare för värden
+//syntax: let namn 
+
+let x  //deklarera variabeln x
+x = 5  //tilldela variabeln x med 5
+
+//x = "hej"
+
+x = x + 1
+
+console.log(x);
+
+let z = "hello" //deklarera + tilldelning
+console.log(z);
+console.log(z+x);
