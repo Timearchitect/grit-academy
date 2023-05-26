4f0b981072afea053c619626ad44c2a0e867a7da
Alrik He <alrik_he@hotmail.com>
2023-05-26 18:26:21 +0200

    first

---
+++ preperandkurs/demo/tillfälle 1/script/script.js
@@ -0,0 +1,22 @@
+// alert( 11 )
+// document.write( 22 )
+// console.log( 33 )
+
+/*  
+alert( 11 )
+alert( 11 )
+alert( 11 )
+*/
+
+// (1) Variabler
+
+let money 
+money = 10
+//money = "hej"
+money = money - 1
+console.log( money )
+
+document.write( money )
+
+
+let alrik = 888
