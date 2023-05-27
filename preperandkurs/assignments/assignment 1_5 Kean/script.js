639b2f88581a40cddf8af7932ba83492bdf972bf
Kean von Wowern <134499865+KeanvonWowernWroblewski@users.noreply.github.com>
2023-05-27 13:50:08 +0200

    Assignment 1_5 in da club Kean

---
+++ preperandkurs/assignments/assignment 1_5 Kean/script.js
@@ -0,0 +1,16 @@
+let age =17
+
+function checkAccess() {
+    let vip = document.getElementById("vip").checked;
+    let age = parseInt(document.getElementById("age").value);
+  
+    if (vip) {
+      alert("Welcome! You are a VIP customer.");
+    } else {
+      if (age >= 18) {
+        alert("Access granted.");
+      } else {
+        alert("Access denied.");
+      }
+    }
+  }
