3be6739481c6b5d50f26cc7000a4655e3d6aa828
Alrik He <alrik_he@hotmail.com>
2023-05-29 10:17:55 +0200

    feedback

--- preperandkurs/assignments/Assignment 1_5 Carolin Hansson/scripts/script.js
+++ preperandkurs/assignments/Assignment 1_5 Carolin Hansson/scripts/script.js
@@ -1,18 +1,18 @@
-let age = 17
-let vip = true
+let age = 17;
+let vip = true;
 
-if (age >= 18) {
-  alert("Access granted");
-  console.log("Access granted");
+if (vip) {
+    alert("Welcome my best VIP costumer!");
+    console.log("welcome my best vip costumer!");
 } else {
-  alert("Access denied");
-  console.warn("Access denied");
-}
+    alert("You are not on the vip list!");
+    console.log("You are not on the vip list!");
 
-if (vip == true) {
-  alert("Welcome my best VIP costumer!");
-  console.log("welcome my best vip costumer!");
-} else {
-  alert("You are not on the vip list!");
-  console.log("You are not on the vip list!");
+    if (age >= 18) {
+        alert("Access granted");
+        console.log("Access granted");
+    } else {
+        alert("Access denied");
+        console.warn("Access denied");
+    }
 }
--- preperandkurs/assignments/assignment 1_5 Kean/script.js
+++ preperandkurs/assignments/assignment 1_5 Kean/script.js
@@ -1,16 +1,26 @@
-let age =17
+let age = 17
 
 function checkAccess() {
     let vip = document.getElementById("vip").checked;
     let age = parseInt(document.getElementById("age").value);
   
-    if (vip) {
+    // if (vip) {
+    //   alert("Welcome! You are a VIP customer.");
+    // } else {
+    //   if (age >= 18) {
+    //     alert("Access granted.");
+    //   } else {
+    //     alert("Access denied.");
+    //   }
+    // }
+
+    //ctrl + * toogle kommentarer //  
+
+    if (vip) 
       alert("Welcome! You are a VIP customer.");
-    } else {
-      if (age >= 18) {
-        alert("Access granted.");
-      } else {
-        alert("Access denied.");
-      }
-    }
+    else if (age >= 18) 
+      alert("Access granted.");
+    else 
+      alert("Access denied."); 
+
   }
