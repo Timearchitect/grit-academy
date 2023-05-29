4863020d77480306e347fbfa373a075c3d24c12e
Alrik He <alrik_he@hotmail.com>
2023-05-29 19:38:11 +0200

    feedback

--- preperandkurs/assignments/Assignment 1.5 Keikiet Pham/script/script.js
+++ preperandkurs/assignments/Assignment 1.5 Keikiet Pham/script/script.js
@@ -2,22 +2,17 @@ function handleClick() {
     let age = document.getElementById("ageInput").value;
     let vip = document.getElementById("vipCheckbox").checked;
 
-    parseInt(age)
+    parseInt(age);
+// alt + shfit +f  format document
+// ctrl + * komment
 
-    if(vip) {
+    if (vip) {
         alert("Welcome in my best VIP customer:)");
-        }
-
-    else if(age >= 18 && vip == false) {
+    } else if (age >= 18) {
         alert("Access granted");
-        
+    } else {
+        alert("Access denied");
     }
-    
-    else {
-    alert("Access denied");
-
-    }
-    
 }
 
 document.getElementById("enterBtn").addEventListener("click", handleClick);
