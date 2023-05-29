a753a8d09f511b073fb810604798e7b9eb917bee
carolin hansson <123896610+CarLouHan@users.noreply.github.com>
2023-05-29 09:54:38 +0200

    Update script.js

--- preperandkurs/assignments/Assignment 1_5 Carolin Hansson/scripts/script.js
+++ preperandkurs/assignments/Assignment 1_5 Carolin Hansson/scripts/script.js
@@ -3,16 +3,16 @@ let vip = true
 
 if (age >= 18) {
   alert("Access granted");
-  console.log("Acess granted");
+  console.log("Access granted");
 } else {
   alert("Access denied");
-  console.warn("acess denied");
+  console.warn("Access denied");
 }
 
 if (vip == true) {
   alert("Welcome my best VIP costumer!");
   console.log("welcome my best vip costumer!");
 } else {
-  alert("You are not on the vip list"!)
-  console.log("You are not on the vip list!")
+  alert("You are not on the vip list!");
+  console.log("You are not on the vip list!");
 }
