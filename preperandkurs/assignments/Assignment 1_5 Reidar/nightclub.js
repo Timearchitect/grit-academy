2404b9f0814b5e52741fb65cdc8478e41a6f4d24
slaystarthunder <72455891+slaystarthunder@users.noreply.github.com>
2023-05-29 10:28:50 +0200

    Add files via upload

--- preperandkurs/assignments/Assignment 1_5 Reidar/nightclub.js
+++ preperandkurs/assignments/Assignment 1_5 Reidar/nightclub.js
@@ -1,10 +1,23 @@
-let vip = true
-let age = 18
+let vip = false;
+let age = 0;
 
-if (vip === true) {
-    console.log("Welcome my best VIP customer!")
-} else if (age >= 18) {
-    console.log("Access granted")
-} else {
-    console.log("Access denied")
-}
+const inputAge = document.getElementById("inputAge");
+const checkboxVip = document.getElementById("checkboxVip");
+
+inputAge.addEventListener("input", function() {
+    age = parseInt(inputAge.value);
+});
+
+checkboxVip.addEventListener("change", function() {
+    vip = checkboxVip.checked;
+});
+
+document.getElementById("enterBtn").addEventListener("click", function() {
+    if (vip === true) {
+        window.location.href = "vip-page-lp.html";
+    } else if (age >= 18) {
+        window.location.href = "access-granted-lp.html";
+    } else {
+        window.location.href = "access-denied-lp.html";
+    }
+});
