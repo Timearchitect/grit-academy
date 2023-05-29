1eb72937709d36e68f804ad15fd6f306c0190463
carolin hansson <123896610+CarLouHan@users.noreply.github.com>
2023-05-29 09:44:11 +0200

    Add files via upload

---
+++ preperandkurs/assignments/Assignment 1_5 Carolin Hansson/scripts/script.js
@@ -0,0 +1,18 @@
+let age = 17
+let vip = true
+
+if (age >= 18) {
+  alert("Access granted");
+  console.log("Acess granted");
+} else {
+  alert("Access denied");
+  console.warn("acess denied");
+}
+
+if (vip == true) {
+  alert("Welcome my best VIP costumer!");
+  console.log("welcome my best vip costumer!");
+} else {
+  alert("You are not on the vip list"!)
+  console.log("You are not on the vip list!")
+}
