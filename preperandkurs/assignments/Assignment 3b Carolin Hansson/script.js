93c74329f2f77bf703d21f0343f9e69ba991dbff
carolin hansson <123896610+CarLouHan@users.noreply.github.com>
2023-06-05 00:12:04 +0200

    Add files via upload

---
+++ preperandkurs/assignments/Assignment 3b Carolin Hansson/script.js
@@ -0,0 +1,13 @@
+
+
+
+let cat = {alive:true, name:"Gustav"}
+
+function showNameStatus () {
+    if (Math.random() <0.5) 
+        alert(cat.name + " is alive!");
+        else
+        (alert (cat.name + " is dead..."));
+    }
+    
+
