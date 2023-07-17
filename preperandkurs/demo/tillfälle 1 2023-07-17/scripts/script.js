91ce9a63438e86b9be8f35fc4c58eace2945e070
Alrik He <alrik_he@hotmail.com>
2023-07-17 12:01:48 +0200

    tillfälle 1 2023-07-17

---
+++ preperandkurs/demo/tillfälle 1 2023-07-17/scripts/script.js
@@ -0,0 +1,37 @@
+/* alert("HELLO!!!")
+document.write("hej")
+console.log("san!!!!") 
+
+test koder 
+
+
+//console.log("san!!!!"); console.log("san!!!!"); 
+console.log("san!!!!");// console.log("san!!!!");
+console.log("san!!!!"); console.log("san!!!!");
+*/
+
+// 1) variabler
+
+
+let foo //skapa en variabel foo = deklarerar
+foo = 123 //lägg in ett värde = tilldela
+
+console.log(foo);
+foo = "Hello"
+console.log(foo);
+
+let bar = 5  //deklaration & tilldelning
+bar = bar + 1
+
+
+console.log( 5 + 5 ); // beräknar
+console.log( "hej" + "san" ); //konkatineras
+console.log( "5" + "5" ); //konkatineras
+console.log( (2 + 3) * 2 ); // beräknar
+// P E M D A S
+//parentess , Exponent , Multiplikation , Divition , Addition , Subtraktion 
+
+console.log("hej11" ); //konkatineras
+
+
+
