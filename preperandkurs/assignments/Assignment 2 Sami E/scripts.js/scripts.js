c722658b1afad8a9e6a56fd2ffc56687f2f68a2c
sami-elt <139684759+sami-elt@users.noreply.github.com>
2023-07-23 17:00:21 +0200

    Add files via upload

---
+++ preperandkurs/assignments/Assignment 2 Sami E/scripts.js/scripts.js
@@ -0,0 +1,6 @@
+let cityNames = ["kiruna", "uppsala", "malmo", "gothenburg", "stockholm"];
+for (let i = 0; i < cityNames.length; i++) {
+  for (let j = 0; j < cityNames[i].length; j++) {
+    console.log(cityNames[i][j]);
+  }
+}
