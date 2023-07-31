05a571561ce0424c525680c20bec64ff571d3e93
Alrik He <alrik_he@hotmail.com>
2023-07-31 12:01:12 +0200

    tillfälle 1 2023-07-31

---
+++ preperandkurs/demo/tillfälle 1 2023-07-31/scripts/script.js
@@ -0,0 +1,36 @@
+/*
+ alert(123)
+document.write("hello world")
+*/
+
+// console.log("Tjenare");
+
+// (1) Variabler  
+// är en behållare för värden
+// syntax grammatik
+
+let foo  // deklarera / skapa en variabel som heter foo
+foo = 5  // tilldeldelning / lägger in ett värde i foo
+
+foo = "hej"
+
+foo = (1 + 1) * 3 // operatorer , operand
+// Parentess Exponent Multiplikation Divition Addition Subtraktion
+foo = "hej " + "san" // concatination/ konkatination
+
+foo = "hej"+(1+1)
+
+
+// 
+let bar = 5  //deklarera + tilldela
+bar = bar + 1 // ökar med 1
+bar += 1      // ökar med 1
+bar ++        // ökar bara med 1 (funkar med nummer)
+
+
+bar = "hej "
+bar = bar + "hej "
+bar += "hej "
+
+
+console.log(foo);
