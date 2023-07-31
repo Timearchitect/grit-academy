3b780d0392454c9bacf2b7fdea49c6a1d266c820
Alrik He <alrik_he@hotmail.com>
2023-07-31 12:03:19 +0200

    deletes stuff

--- preperandkurs/assignments/assigment 2 støder/script.js
+++
@@ -1,8 +0,0 @@
-let cities = ["Kiruna", "Stockholm", "Gothenburg"];
-
-const randomIndex = Math.floor(Math.random() * cities.length) ;
-
-const removedCity = cities.splice(randomIndex, 1)[0];
-
-console.log('Removed city')
-console.log('Remaining citites')
