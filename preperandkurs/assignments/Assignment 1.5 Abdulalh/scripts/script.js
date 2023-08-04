8bdf91d35e7e20f8c4589452ad265bb3fe6d07f9
Abmeiister <141013991+Abmeiister@users.noreply.github.com>
2023-08-04 11:50:59 +0200

    abdullah uppgift 1,5

---
+++ preperandkurs/assignments/Assignment 1.5 Abdulalh/scripts/script.js
@@ -0,0 +1,10 @@
+var vip = true;
+var age = 17
+if (age>=18) {
+    alert("Access granted")}
+    else if (age<18)  {
+    alert("Access denied")
+}
+if (vip = true ) {
+    alert("Welcome in my best VIP customer :)")
+}
