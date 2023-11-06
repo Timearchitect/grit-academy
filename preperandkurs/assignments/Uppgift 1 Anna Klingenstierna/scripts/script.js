ae000aaaec6dd9c74801e2b6baa958ac1c5b9574
Alrik He <alrik_he@hotmail.com>
2023-11-06 11:29:46 +0100

    anna flytta

---
+++ preperandkurs/assignments/Uppgift 1 Anna Klingenstierna/scripts/script.js
@@ -0,0 +1,13 @@
+//kommentar- datorn ignonerar detta
+// variabel är en behållare, kan bara innehålla 1 sak 
+
+alert ("You can´t divide us!")
+
+let foo = 0
+let bar = 0 
+console.log ( foo/bar ) 
+
+if (foo/bar != NaN) {
+  console.log ("You cant´t divide us")
+}
+  
