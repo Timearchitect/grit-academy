8126be20784b7049a3bcbb1fa8f39ebd32186149
meehti <mattidehdar@live.se>
2023-11-06 10:23:34 +0100

    test
    test

---
+++ preperandkurs/demo/tillfälle 1 Motiollah/tillfälle 1/scripts.js
@@ -0,0 +1,38 @@
+// alert(777);
+console.log(888);
+console.log("console");
+
+//deklarera en variabel foo
+let foo; 
+//tilldela värdet 12
+foo = 12;
+
+let bar = 5;
+
+bar = bar + 5;  //ökar med 5
+bar += 5;       //ökar med 5
+bar ++;         //ökar med 1
+
+
+//bar = 3;        // byte (byte)      ,byte 
+bar = 12 ;      // integer (int)    ,heltal
+bar = 0.245;    // float (float)    ,decimal tal
+bar = 'c';      // character (char) ,karaktär
+bar = true;     // boolean (bool)   ,sant/falskt
+bar = "hejsan!" // String (string)  ,text
+
+
+
+
+// array är en hylla
+// deklarara en array  och tilldela 5 element
+          // 0     1    2    3        4
+let foo2 = [ 12 ,0.256,'c',"hejsan!",true] ;
+foo2[1] = "hello";
+
+//foo2.push("Alrik!!");  
+//foo2.pop();
+
+console.log( foo2.length );
+
+
---
+++ preperandkurs/demo/tillfälle 1 Motiollah/tillfälle 1/scripts/scripts.js
@@ -0,0 +1,11 @@
+
+let foo; 
+foo=0;
+
+
+let bar
+bar=0;
+
+console.log(foo/bar);
+
+
