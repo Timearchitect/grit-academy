7177595987520e4577ea6bad5e00e89cd8839623
Alrik He <alrik_he@hotmail.com>
2023-11-06 10:18:13 +0100

    sdfsdf

---
+++ preperandkurs/demo/tillfälle 1 2023-11-03/javascripts/script.js
@@ -0,0 +1,22 @@
+
+// kommenter
+//alert(123)
+//document.write(456)
+//console.log(789) // debugga lätt
+
+
+//variabel är en behållare
+let x   //deklarerar en variabel som heter x
+
+x = 50  //tilldelar 50 till x
+
+x = 100 
+
+
+let namn = "Alrik"
+
+//namn = x * 2 + 50 // den utför kalkulationer före
+
+console.log( namn + (3 + 3) )
+
+console.log( "hej"+ "san" )
