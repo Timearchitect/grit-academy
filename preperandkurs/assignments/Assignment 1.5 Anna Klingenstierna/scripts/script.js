44d8b97e1387710a6b146ca5d712837198d0f088
AnnaKlingenstierna <149709837+AnnaKlingenstierna@users.noreply.github.com>
2023-11-07 21:14:05 +0100

    Add files via upload

---
+++ preperandkurs/assignments/Assignment 1.5 Anna Klingenstierna/scripts/script.js
@@ -0,0 +1,57 @@
+let age = 17
+age = 17
+
+let vip = true
+vip = true // datatypen är boolean
+ // datatypen är integer
+ 
+ if (vip) {
+  alert ("Welcome in my best VIP costumer:)")
+  
+}
+else if  (18 >= age ) {
+  alert ("Access granted")
+
+}
+else {
+  alert ("Access denied")
+}  
+
+
+
+
+
+if (vip) {
+  console.log ("Welcome in my best VIP costumer:)")
+  
+}
+if (18 >= age ||vip == true ) {
+  console.log ("Access granted")
+}
+else {
+
+
+ console.log ("Access denied")
+}
+ 
+let data = document.getElementsByTagName ("input")
+
+
+console.log(data [0].value);
+
+
+
+/* function ålder(vip) {
+  
+}
+
+let test= ["Anna" , 50 , true, 4.40, "hej"] 
+test [7]= "Hejsan" */
+
+/* test [1] =  30
+test [3] = 5
+test [2] = "jag vinner"
+console.log (test [1])
+console.log (test [3])
+console.log (test)
+ */
