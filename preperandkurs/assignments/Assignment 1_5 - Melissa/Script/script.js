c1bb62a58f83aa81733ab3fd56c6486871009512
Meli <melissaruzic@hotmail.com>
2023-11-10 01:04:18 +0100

    assignment 1_5

---
+++ preperandkurs/assignments/Assignment 1_5 - Melissa/Script/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var ageInput = document.getElementById("age");
+    var vipCheckbox = document.getElementById("vip");
+    var enterBtn = document.getElementById("enterBtn");
+
+    enterBtn.addEventListener("click", function() {
+    
+        var age = parseInt(ageInput.value);
+        var vip = vipCheckbox.checked;
+
+        
+        if (vip === true) {
+            
+            alert("Welcome my best VIP customer :)");
+        } else {
+            
+            if (age >= 18) {
+                
+                alert("Access granted");
+            } else {
+                
+                alert("Access denied");
+            }
+        }
+    });
+});
+
+
