b2d4bd859e84d30c7146739c3b10e05ed66ffafa
Alrik He <alrik_he@hotmail.com>
2023-11-27 18:34:23 +0100

    klar med uppgift 1

---
+++ preperandkurs/assignments/tillfälle 1 2023-11-27/script/script.js
@@ -0,0 +1,47 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+
+//variabler är en behållare
+
+let foo // deklarera en variabel som heter foo
+        // skapat en behållare som heter foo
+
+foo = 5 + 1 // tilldela variabeln foo med siffran 5
+        // jag la till 5 i foo
+
+foo = "hej" + (1 + 1)  //tilldelning av en texten hello world
+
+
+console.log(foo );
+
+
+
+
+
+// 2 Datatyper
+let x = true
+
+//             String
+x = "hejsan" // String , sträng , text
+x = '@'      // char , karaktär 
+
+//             number
+x = -5  // integer , heltal
+x = 0.5     // float , decimaltal
+
+//             boolean
+x = false    //boolean, bool
+
+//String + String -> String
+//String + number -> String 
+//String + boolean -> String
+
+//number + number -> number
+//number + boolean -> number
+
+//boolean + boolean -> number
+
+console.log(typeof x)
