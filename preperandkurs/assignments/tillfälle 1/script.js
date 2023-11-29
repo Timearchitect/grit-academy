f67f48697bad90496756c6a7a45584b4b6797afc
LENAhello <152437530+LENAhello@users.noreply.github.com>
2023-11-29 20:38:30 +0100

    lenas uppgift

---
+++ preperandkurs/assignments/tillfälle 1/script.js
@@ -0,0 +1,66 @@
+alert(hi)
+document.write(456)
+console.log(789)
+
+// variabler är en behållare  
+
+let foo // deklarera en variabel som heter foo
+// skapat en behållare som heter foo
+foo = 5 + 1  // tilldela variabeln foo med siffror 
+// jag la till 5 i foo
+foo = "hello world" + "5" 
+
+
+
+console.log(foo + 1);
+
+
+
+// datatyper
+// number 
+let x = 4 // integer, heltal 
+x = 0.5 // float , decimaltal 
+// String 
+x = "hejsan" // String , sträng , text 
+x = "@" // char , karaktär 
+
+// boolean
+ x = false  // boolean , bool
+   
+let hylla // deklarerar en variabel hylla 
+    //  0   1    2       3    4
+hylla = [5, 88, "hej", true , y ]
+hylla[0]= Lena 
+console.log(hylla)
+console.log(hylla [0]) 
+console.log(hylla.lenght)
+let y 
+y = 5 
+
+let namnlista  =[ "linnea", "um" , "Adam"]
+namnlista.reverse()
+
+// if satser 
+
+//(condition)
+
+// < mindre än
+// > stööre än
+// == lika med 
+// <= mindre eller lika med 
+// <= större eller lika med 
+// ! betyder inte 
+// === lika med (samma datatyp)
+
+
+let myAge = 31
+
+let vip = true  
+
+if (myAge >= 18) {
+
+console.log("MINDYG!!")
+
+}else { 
+    console.log("mindreåring")
+}
