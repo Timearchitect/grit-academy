22df49e1ce8885710a6833035f3d8a6fb55d252b
LENAhello <152437530+LENAhello@users.noreply.github.com>
2023-11-30 10:19:17 +0100

    assignment 1.5 lena

---
+++ preperandkurs/assignments/assignment 1.5 lena/javascript.js
@@ -0,0 +1,18 @@
+let age;
+age=17
+
+if (age >=18)
+{
+    console.log ( "access granted!!")
+}
+else { 
+    console.log ("access denied!!")
+}
+let vip;
+vip = true
+
+if (false)
+{ 
+    console.log ( "welcome in My best vip costumer")
+
+}
