d2160609dad50ed63c84de93a5af8958988695ab
Linnea301 <152190993+Linnea301@users.noreply.github.com>
2023-12-01 22:14:50 +0100

    Add files via upload
    Linnea 1_5

---
+++ preperandkurs/assignments/Assignment 1_5 Linnea Rejnefelt/scripts/assignment1_5.js
@@ -0,0 +1,17 @@
+let age = 17;
+let vip = true;
+
+
+if (vip == true) {
+    alert("Welcome in my best VIP customer :)");
+    
+} else if (age >= 18) {
+    alert("Access granted!");
+
+} else (age < 18) ;
+    alert("Access denide!");
+
+
+
+    
+
