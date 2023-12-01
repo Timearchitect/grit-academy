120b2d77fe36e6712fda271a75a00c45b7cc69c8
Adam Holgersson <151858746+AdamHolgersson@users.noreply.github.com>
2023-12-01 11:22:22 +0100

    Assignment 1_5 Adam

---
+++ preperandkurs/assignments/Assignment 1_5 Adam Holgersson/Scripts/script.js
@@ -0,0 +1,10 @@
+let vip = true;
+let age = 18;
+
+if (vip == true) {
+  alert("Welcome in my best VIP customer :)");
+} else if (vip == true || age >= 18) {
+  alert("Access granted");
+} else {
+  alert("Acces denied");
+}
