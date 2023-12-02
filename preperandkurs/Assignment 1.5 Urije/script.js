c10f606257b1f4450d914db4100ae780e1534640
Ulli-m <151999855+Ulli-m@users.noreply.github.com>
2023-12-02 21:17:00 +0100

    Add files via upload

---
+++ preperandkurs/Assignment 1.5 Urije/script.js
@@ -0,0 +1,17 @@
+
+let vip = false; 
+let age = 17
+
+if ( vip==true){
+
+    console.log("Welcome my best VIP customer");
+
+}else { if( age >=18 ){
+
+    console.log("ACCESS GRANTED");
+    
+} else {
+    console.log("ACCESS DENIED");
+}
+
+}
