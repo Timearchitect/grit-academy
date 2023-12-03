4ff1c578525201376c29aa48d90778056c9cdb89
adrian-tu <152728900+adrian-tu@users.noreply.github.com>
2023-12-03 12:52:06 +0100

    adrians uppgift

---
+++ preperandkurs/assignments/tillfälle 1 uppgift 1.1 adrian tu 2023-12-01/script/script.js
@@ -0,0 +1,68 @@
+alert(123)
+document.write(456)
+console.log(789)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  let y
+  y = 5
+
+  let hylla //deklarerar en variabel hylla
+         //0    1    2       3   4
+  hylla = [5 , 88 ,"hej" , true, y]  
+
+  hylla[0] = "Alrik"
+  
+  console.log( hylla[0] );
+  console.log(hylla)
+
+
+
+
+
+
+
+
+
+
