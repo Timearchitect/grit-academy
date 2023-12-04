3c01da77add85095ee4a381ed15a656235b4a042
Alrik He <alrik_he@hotmail.com>
2023-12-04 16:14:35 +0100

    1_5 feedback Linnea

--- preperandkurs/assignments/Assignment 1_5 Linnea Rejnefelt/scripts/assignment1_5.js
+++ preperandkurs/assignments/Assignment 1_5 Linnea Rejnefelt/scripts/assignment1_5.js
@@ -1,17 +1,11 @@
-let age = 17;
-let vip = true;
-
-
-if (vip == true) {
-    alert("Welcome in my best VIP customer :)");
-    
-} else if (age >= 18) {
-    alert("Access granted!");
-
-} else (age < 18) ;
-    alert("Access denide!");
-
-
-
-    
-
+function checkID() {
+    let age = parseInt(document.getElementById("age").value);
+    let vip = document.getElementById("vip").checked;
+    //console.log(typeof age);
+    if (vip == true) 
+        alert("Welcome in my best VIP customer :)");
+    else if (age >= 18) 
+        alert("Access granted!");
+    else 
+        alert("Access denied!");
+}
