e5f686e0269a6e1cf751b2661f8e87289901159b
Alrik He <alrik_he@hotmail.com>
2023-12-11 18:30:54 +0100

    nice

---
+++ preperandkurs/demo/tillfälle 1 2023-12-11/script.js
@@ -0,0 +1,95 @@
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+/*
+console.log(789); console.log(789);
+console.log(789); // console.log(789);
+console.log(789); console.log(789);
+*/
+     
+// kommentarer i js
+
+
+// 1) Variabler
+// behållare som innehåller ett värde
+// syntax
+
+let alrik  // skapar en variabel som heter alrik
+//deklarera variabeln alrik
+//declare variable alrik
+
+alrik = 5 // skriveröver med 5
+//tilldela alrik med siffran 5
+//assign variable alrik with number 5
+
+alrik = 10 + 10
+//kalkylerar när det går
+
+//alrik = "hej" + "san" //text har alltid citattecken " eller singel citat '
+// konkatenation (klistrar ihop text)
+//concatination
+
+alrik = (3 + 2) * 2
+// P -> parantess
+// E -> exponent
+// M -> multiplikation
+// D -> divition
+// A -> addition
+// S -> subtraction
+
+
+alrik = ""+ 1 + 1 +"hej" + 1 + 1   // addition med strings & number
+
+
+
+let alrik2 = -0.5  //deklarera + tilldela
+
+
+
+//console.log(alrik + alrik2);
+
+
+let x = 0
+x = x + 1   // ökar med 1
+x += 1      //samma
+x ++        //samma men bara 1
+console.log(x);
+
+
+
+
+
+
+
+// 2) Datatyper
+let y = false
+
+//string
+y = "bsdfsdfok"  // string, sträng
+y = '©'        // char,charater, karaktär  
+y = `hello sdf
+sddfsdf multiline` // multi line
+
+//number
+y =  -105       // integer,heltal
+y =  0.25       // float/double,decimaltal 
+
+//boolean
+y = true       // bool,boolean,boolean
+
+//object
+y = {  }       //Object
+
+// dominant
+//string + string -> string
+//string + number -> string
+//string + boolean -> string
+
+//number + number -> number
+//number + boolean -> number
+
+// recessive
+//boolean + boolean -> number
+
