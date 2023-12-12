d7cdcabecf61c6f8343c535800057b0a8921385c
KlaraMattiasson <153517312+KlaraMattiasson@users.noreply.github.com>
2023-12-12 21:18:38 +0100

    Add files via upload

---
+++ preperandkurs/assignments/Assignment 1 Klara Strandberg/Script/script.js
@@ -0,0 +1,14 @@
+let foo = 0;
+let bar = 0;
+
+if (foo == 0) {
+    alert('You cant divide us');
+    
+} else if (foo == 0 || bar == 0 ) {
+    alert ('You cant divide us')
+    
+}
+console.log(foo/bar)
+
+
+function log() {console.log('divide')}
