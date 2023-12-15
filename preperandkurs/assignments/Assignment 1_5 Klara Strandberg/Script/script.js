8e4bb17a93f8a72651b8c8e6a56cd800ea956738
KlaraMattiasson <153517312+KlaraMattiasson@users.noreply.github.com>
2023-12-15 10:20:56 +0100

    Add files via upload

---
+++ preperandkurs/assignments/Assignment 1_5 Klara Strandberg/Script/script.js
@@ -0,0 +1,17 @@
+let age = 18;
+let vip = true;
+
+if ( vip == true ){ 
+     
+    alert ("Welcome my best VIP customer") 
+
+     if ( age => 18 || vip == true) {
+        alert ("Acces granted");
+     } else {
+        alert ("Access denied");
+     }
+        console.log (age, vip)
+
+
+}
+
