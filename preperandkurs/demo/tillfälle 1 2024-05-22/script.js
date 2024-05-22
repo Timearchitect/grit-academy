f92d6047353881c6f505505789db34505aa81af0
Alrik He <alrik_he@hotmail.com>
2024-05-22 12:57:01 +0200

    Add files via upload

---
+++ preperandkurs/demo/tillfälle 1 2024-05-22/script.js
@@ -0,0 +1,34 @@
+/*
+
+alert(123)
+document.write(456)
+console.log(789)
+
+*/
+
+
+//höger om // är kommentar
+
+// 1) variabler är en behållare
+let x   //skapa en variabel x
+x = 5   //lägga in siffran 5 till variabeln x
+
+x = "hej" // lägger in texten hej in till x 
+
+x = 1 + 1
+
+x = x 
+
+x = "hej" + "san"
+
+x = "hej" + (1 + 1)  // hej2
+
+x =  1 + 1 + "hej" // 2hej
+
+x = (2 + 2) * 3 
+
+
+let y = 5 //deklaration + tilldelning
+
+
+console.log( x + y )
